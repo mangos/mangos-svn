aba913ab53ed6168525ee7a28e4605771221e551
theluda <theluda@no-mail.dom>
2005-09-01 17:20:49 +0000

    [2] MaNGOS Project forked from WoW Daemon in 2005

--- src/shared/ByteBuffer.h
+++ src/shared/ByteBuffer.h
@@ -2,6 +2,7 @@
  *
  * Copyright (C) 2004 quetzal
  * Copyright (C) 2004 Wow Daemon
+ * Copyright (C) 2005 MaNGOS <https://opensvn.csie.org/traccgi/MaNGOS/trac.cgi/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
