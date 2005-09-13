b021d216e43114e43f7da98e660c4dfd880972d1
theluda <theluda@26674162-90ff-0310-a85b-b64107c8dee9>
2005-09-13 18:48:01 +0000

    Added default data for realm list
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@46 26674162-90ff-0310-a85b-b64107c8dee9

---
+++ sql/data/realms.sql
@@ -0,0 +1,17 @@
+-- phpMyAdmin SQL Dump
+-- version 2.6.2
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: Sep 13, 2005 at 08:46 PM
+-- Server version: 4.1.12
+-- PHP Version: 5.0.4
+-- 
+-- Database: `mangos`
+-- 
+
+-- 
+-- Dumping data for table `realms`
+-- 
+
+INSERT INTO `realms` VALUES (1, 'Man''Gos', '127.0.0.1:8129', 1, 0, 1);
