25b9f9e7836cd998b556365bc85a5a5c84101a8c
siuolly <siuolly@26674162-90ff-0310-a85b-b64107c8dee9>
2005-09-15 16:46:37 +0000

    Change define tag.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@66 26674162-90ff-0310-a85b-b64107c8dee9

--- src/shared/Errors.h
+++ src/shared/Errors.h
@@ -18,8 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef WOWSERVER_ERRORS_H
-#define WOWSERVER_ERRORS_H
+#ifndef MANGOSSERVER_ERRORS_H
+#define MANGOSSERVER_ERRORS_H
 
 // TODO: handle errors better
 
