149f9963571e1f655fd3f42fb73b4ded7610136c
siuolly <siuolly@26674162-90ff-0310-a85b-b64107c8dee9>
2005-09-19 16:20:29 +0000

    Some minor changes to MapManager.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@99 26674162-90ff-0310-a85b-b64107c8dee9

--- src/game/MapManager.cpp
+++ src/game/MapManager.cpp
@@ -1,15 +1,27 @@
 
 #include "MapManager.h"
 
+MapManager::MapManager() : i_zones(MAX_ZONES)
+{
+  std::fill(i_zones.begin(), i_zones.end(), NULL);
+}
+
+MapManager::~MapManager()
+{
+  for(ZonesType::iterater iter=i_zones.begin(); iter != i_zones.end(); ++iter)
+    delete *iter;
+  i_zones.clear();
+}
+
 void
 MapManager::PlayerEnterLocation(Player *player, const uint32 &zone_id, const float &x, const float &y)
 {
-    if( !i_zoneStatus.test(zone_id) )
+  if( i_zones[zone_id] == NULL )
     {    
       Guard guard;
-      if( !i_zoneStatus.test(zone_id) )
+      if( i_zones[zone_id] == NULL )
 	{
-	  Zone = new Zone;
+	  Zone<MAX_GRIDS_PER_ZONE> zone = new Zone<MAX_GRIDS_PER_ZONE>;
 	}
     }
 }
