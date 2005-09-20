02e10ddf273d91c43839326a44ddb355c0239143
siuolly <siuolly@no-mail.dom>
2005-09-20 22:12:37 +0000

    [105] Fix up the table syntax error.

--- sql/playercreateinfo_actions.sql
+++ sql/playercreateinfo_actions.sql
@@ -1,11 +1,11 @@
-#----------------------------
-# Table structure for playercreateinfo
-#----------------------------
-CREATE TABLE `playercreateinfo_actions` (
+-----------------------------
+-- Table structure for playercreateinfo
+-----------------------------
+DROP TABLE IF EXISTS `playercreateinfo_actions`;
+CREATE TABLE IF NOT EXISTS  `playercreateinfo_actions` (
   `createId` tinyint(3) unsigned NOT NULL default '0',
   `button` smallint(2) unsigned NOT NULL default '0',
   `action` smallint(6) unsigned NOT NULL default '0',
   `type` smallint(3) unsigned NOT NULL default '0',
-  `misc` smallint(3) unsigned NOT NULL default '0',
-
+  `misc` smallint(3) unsigned NOT NULL default '0'
 ) TYPE=MyISAM;
--- sql/playercreateinfo_spells.sql
+++ sql/playercreateinfo_spells.sql
@@ -1,8 +1,9 @@
-#----------------------------
-# Table structure for playercreateinfo_spells
-#----------------------------
-CREATE TABLE `playercreateinfo_spells` (
+-----------------------------
+-- Table structure for playercreateinfo_spells
+-----------------------------
+DROP TABLE IF EXISTS `playercreateinfo_spells`;
+CREATE TABLE IF NOT EXISTS `playercreateinfo_spells` (
   `createId` tinyint(3) unsigned NOT NULL default '0',
-  `spell` mediumint(8) unsigned NOT NULL default '0',
-
+  `spell` mediumint(8) unsigned NOT NULL default '0'
 ) TYPE=MyISAM;
+
