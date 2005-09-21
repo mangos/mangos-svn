976ae3b096a41b2bde13ce8c179230a0e554e4ef
nostre <nostre@no-mail.dom>
2005-09-21 18:09:15 +0000

    [116]

--- sql/data/playercreateinfoData.sql
+++ sql/data/playercreateinfoData.sql
@@ -51,150 +51,150 @@ VALUES
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (11, 4, 3, 1, 141, 10311.3, 832.463, 1326.41, 55, 17, 28, 20, 20, 21, 46, 85, 0, 0, 0, 26, 4, 5);
+  (15, 4, 3, 1, 141, 10311.3, 832.463, 1326.41, 55, 17, 28, 20, 20, 21, 46, 85, 0, 0, 0, 26, 4, 5);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (12, 2, 3, 1, 14, -618.518, -4251.67, 38.718, 51, 23, 20, 23, 17, 24, 76, 82, 0, 0, 0, 25, 4, 5);
+  (11, 2, 3, 1, 14, -618.518, -4251.67, 38.718, 51, 23, 20, 23, 17, 24, 76, 82, 0, 0, 0, 25, 4, 5);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (13, 3, 3, 0, 1, -6240.32, 331.033, 382.758, 53, 22, 19, 24, 19, 20, 86, 84, 0, 0, 0, 24, 4, 5);
+  (12, 3, 3, 0, 1, -6240.32, 331.033, 382.758, 53, 22, 19, 24, 19, 20, 86, 84, 0, 0, 0, 24, 4, 5);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (14, 6, 3, 1, 215, -2917.58, -257.98, 52.9968, 59, 25, 18, 23, 15, 23, 76, 80, 0, 0, 0, 16, 5, 7);
+  (16, 6, 3, 1, 215, -2917.58, -257.98, 52.9968, 59, 25, 18, 23, 15, 23, 76, 80, 0, 0, 0, 16, 5, 7);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (15, 8, 3, 1, 14, -618.518, -4251.67, 38.718, 1478, 21, 25, 22, 16, 22, 66, 81, 0, 0, 0, 16, 5, 7);
+  (17, 8, 3, 1, 14, -618.518, -4251.67, 38.718, 1478, 21, 25, 22, 16, 22, 66, 81, 0, 0, 0, 16, 5, 7);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (16, 1, 4, 0, 12, -8949.95, -132.493, 83.5312, 49, 21, 23, 21, 20, 20, 55, 0, 0, 0, 100, 27, 10, 13);
+  (18, 1, 4, 0, 12, -8949.95, -132.493, 83.5312, 49, 21, 23, 21, 20, 20, 55, 0, 0, 0, 100, 27, 10, 13);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (17, 2, 4, 1, 14, -618.518, -4251.67, 38.718, 51, 24, 20, 23, 17, 23, 75, 0, 0, 0, 100, 30, 4, 4);
+  (19, 2, 4, 1, 14, -618.518, -4251.67, 38.718, 51, 24, 20, 23, 17, 23, 75, 0, 0, 0, 100, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (18, 3, 4, 0, 1, -6240.32, 331.033, 382.758, 53, 23, 19, 24, 19, 19, 85, 0, 0, 0, 100, 30, 4, 4);
+  (20, 3, 4, 0, 1, -6240.32, 331.033, 382.758, 53, 23, 19, 24, 19, 19, 85, 0, 0, 0, 100, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (19, 4, 4, 1, 141, 10311.3, 832.463, 1326.41, 55, 18, 28, 20, 20, 20, 45, 0, 0, 0, 100, 30, 4, 4);
+  (21, 4, 4, 1, 141, 10311.3, 832.463, 1326.41, 55, 18, 28, 20, 20, 20, 45, 0, 0, 0, 100, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (20, 5, 4, 0, 85, 1676.35, 1677.45, 121.67, 57, 20, 21, 22, 18, 25, 65, 0, 0, 0, 100, 30, 4, 4);
+  (22, 5, 4, 0, 85, 1676.35, 1677.45, 121.67, 57, 20, 21, 22, 18, 25, 65, 0, 0, 0, 100, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (21, 7, 4, 0, 1, -6340.32, 331.033, 382.758, 1563, 16, 26, 20, 23, 20, 45, 0, 0, 0, 100, 30, 4, 4);
+  (23, 7, 4, 0, 1, -6340.32, 331.033, 382.758, 1563, 16, 26, 20, 23, 20, 45, 0, 0, 0, 100, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (22, 8, 4, 1, 14, -618.518, -4251.67, 38.718, 1478, 22, 25, 22, 16, 21, 65, 0, 0, 0, 100, 30, 4, 4);
+  (24, 8, 4, 1, 14, -618.518, -4251.67, 38.718, 1478, 22, 25, 22, 16, 21, 65, 0, 0, 0, 100, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (23, 1, 5, 0, 12, -8949.95, -132.493, 83.5312, 49, 20, 20, 20, 22, 23, 52, 160, 0, 0, 0, 30, 4, 4);
+  (25, 1, 5, 0, 12, -8949.95, -132.493, 83.5312, 49, 20, 20, 20, 22, 23, 52, 160, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (24, 3, 5, 0, 1, -6240.32, 331.033, 382.758, 53, 22, 16, 23, 21, 22, 82, 145, 0, 0, 0, 30, 4, 4);
+  (26, 3, 5, 0, 1, -6240.32, 331.033, 382.758, 53, 22, 16, 23, 21, 22, 82, 145, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (25, 4, 5, 1, 141, 10311.3, 832.463, 1326.41, 55, 17, 25, 19, 22, 23, 51, 160, 0, 0, 0, 30, 4, 4);
+  (27, 4, 5, 1, 141, 10311.3, 832.463, 1326.41, 55, 17, 25, 19, 22, 23, 51, 160, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (26, 5, 5, 0, 85, 1676.35, 1677.45, 121.67, 57, 19, 18, 21, 20, 28, 62, 130, 0, 0, 0, 30, 4, 4);
+  (28, 5, 5, 0, 85, 1676.35, 1677.45, 121.67, 57, 19, 18, 21, 20, 28, 62, 130, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (27, 8, 5, 1, 14, -618.518, -4251.67, 38.718, 1478, 21, 22, 21, 18, 24, 62, 128, 0, 0, 0, 30, 4, 4);
+  (29, 8, 5, 1, 14, -618.518, -4251.67, 38.718, 1478, 21, 22, 21, 18, 24, 62, 128, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (28, 2, 7, 1, 14, -618.518, -4251.67, 38.718, 51, 24, 17, 23, 18, 25, 77, 73, 0, 0, 0, 30, 4, 4);
+  (30, 2, 7, 1, 14, -618.518, -4251.67, 38.718, 51, 24, 17, 23, 18, 25, 77, 73, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (29, 6, 7, 1, 215, -2917.58, -257.98, 52.9968, 59, 26, 15, 23, 16, 24, 77, 71, 0, 0, 0, 30, 4, 4);
+  (31, 6, 7, 1, 215, -2917.58, -257.98, 52.9968, 59, 26, 15, 23, 16, 24, 77, 71, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (30, 8, 7, 1, 14, -618.518, -4251.67, 38.718, 1478, 22, 22, 22, 17, 23, 67, 72, 0, 0, 0, 30, 4, 4);
+  (32, 8, 7, 1, 14, -618.518, -4251.67, 38.718, 1478, 22, 22, 22, 17, 23, 67, 72, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (31, 1, 8, 0, 12, -8949.95, -132.493, 83.5312, 49, 20, 20, 20, 23, 22, 52, 165, 0, 0, 0, 30, 4, 4);
+  (34, 1, 8, 0, 12, -8949.95, -132.493, 83.5312, 49, 20, 20, 20, 23, 22, 52, 165, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (32, 5, 8, 0, 85, 1676.35, 1677.45, 121.67, 57, 19, 18, 21, 21, 27, 62, 135, 0, 0, 0, 30, 4, 4);
+  (35, 5, 8, 0, 85, 1676.35, 1677.45, 121.67, 57, 19, 18, 21, 21, 27, 62, 135, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (33, 7, 8, 0, 1, -6340.32, 331.033, 382.758, 1563, 15, 23, 19, 26, 22, 51, 210, 0, 0, 0, 30, 4, 4);
+  (36, 7, 8, 0, 1, -6340.32, 331.033, 382.758, 1563, 15, 23, 19, 26, 22, 51, 210, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (34, 8, 8, 1, 14, -618.518, -4251.67, 38.718, 1478, 21, 22, 21, 19, 23, 62, 119, 0, 0, 0, 30, 4, 4);
+  (37, 8, 8, 1, 14, -618.518, -4251.67, 38.718, 1478, 21, 22, 21, 19, 23, 62, 119, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (35, 1, 9, 0, 12, -8949.95, -132.493, 83.5312, 49, 20, 20, 21, 22, 22, 53, 140, 0, 0, 0, 30, 4, 4);
+  (38, 1, 9, 0, 12, -8949.95, -132.493, 83.5312, 49, 20, 20, 21, 22, 22, 53, 140, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (36, 2, 9, 1, 14, -618.518, -4251.67, 38.718, 51, 23, 17, 23, 19, 25, 73, 109, 0, 0, 0, 30, 4, 4);
+  (39, 2, 9, 1, 14, -618.518, -4251.67, 38.718, 51, 23, 17, 23, 19, 25, 73, 109, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (37, 5, 9, 0, 85, 1676.35, 1677.45, 121.67, 57, 19, 18, 22, 20, 27, 63, 110, 0, 0, 0, 30, 4, 4);
+  (40, 5, 9, 0, 85, 1676.35, 1677.45, 121.67, 57, 19, 18, 22, 20, 27, 63, 110, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (38, 7, 9, 0, 1, -6340.32, 331.033, 382.758, 1563, 15, 23, 20, 25, 22, 43, 185, 0, 0, 0, 30, 4, 4);
+  (41, 7, 9, 0, 1, -6340.32, 331.033, 382.758, 1563, 15, 23, 20, 25, 22, 43, 185, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (39, 4, 11, 1, 141, 10311.3, 832.463, 1326.41, 55, 18, 25, 19, 22, 22, 53, 100, 0, 0, 0, 30, 4, 4);
+  (42, 4, 11, 1, 141, 10311.3, 832.463, 1326.41, 55, 18, 25, 19, 22, 22, 53, 100, 0, 0, 0, 30, 4, 4);
 
 INSERT INTO playercreateinfo
   (createId, race, class, mapID, zoneID, positionX, positionY, positionZ, displayID, BaseStrength, BaseAgility, BaseStamina, BaseIntellect, BaseSpirit, BaseHealth, BaseMana, BaseRage, BaseFocus, BaseEnergy, attackpower, mindmg, maxdmg)
 VALUES
-  (40, 6, 11, 1, 215, -2917.58, -257.98, 52.9968, 59, 26, 15, 22, 17, 24, 74, 67, 0, 0, 0, 30, 4, 4);
+  (43, 6, 11, 1, 215, -2917.58, -257.98, 52.9968, 59, 26, 15, 22, 17, 24, 74, 67, 0, 0, 0, 30, 4, 4);
 
