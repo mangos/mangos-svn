3a8c55baf362b6064820b1df3d39e2c369b20fac
siuolly <siuolly@no-mail.dom>
2005-09-21 01:59:19 +0000

    [107] Add ObjectGridLoader, implements the visitor pattern for GridLoader.

---
+++ src/game/ObjectGridLoader.h
@@ -0,0 +1,69 @@
+/* ObjectGridLoader.h
+ *
+ * Copyright (C) 2005 MaNGOS <https://opensvn.csie.org/traccgi/MaNGOS/trac.cgi/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef MANGOS_OBJECTGRIDLOADER_H
+#define MANGOS_OBJECTGRIDLOADER_H
+
+#include "Grid.h"
+#include "Player.h"
+#include "GameObject.h"
+#include "Creature.h"
+
+#define TYPE_LIST_2(GameObject, Creature)    AllObjectTypes
+
+/*
+ * @class ObjectGridLoader class implements a visitor patter for the ContainerMapList
+ * becuase that's the container used for storing both GameObjects and Creatures
+ * what's in the grid.
+ */
+
+typedef Grid<Player, AllObjectTypes> GridType;
+
+class MANGOS_DLL_DECL ObjectGridLoader
+{
+public:
+    ObjectGridLoader(GridType &grid, Player *pl) : i_grid(grid), i_player(*pl) {}
+    void Visit(std::map<OBJECT_HANDLE, GameObject *> &m);
+    void Visit(std::map<OBJECT_HANDLE, Creature *> &m);
+
+private:
+    GridType &i_grid;
+    Player &i_player;
+};
+
+/*
+ * @class ObjectGridUnloader also implements the visitor pattern
+ * for unloading the grid.
+ */
+class MANGOS_DLL_DELC ObjectGridUnloader
+{
+public:
+    ObjectGridUnloader(GridType &grid, Player *pl) : i_grid(grid), i_player(*pl) {}
+
+    void Visit(std::map<OBJECT_HANDLE, GameObject *> &m);
+    void Visit(std::map<OBJECT_HANDLE, Creature *> &m);
+
+private:
+    GridType &i_grid;
+    Player &i_player;
+};
+
+typedef GridLoader<Player, AllObjecTypes, ObjectGridLoader, ObjectGridUnloader> GridLoaderType;
+
+#endif
