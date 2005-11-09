57e4f4eda25834304dd6b10620cf333c3f5d308a
bubbles <bubbles@no-mail.dom>
2005-11-09 01:29:52 +0000

    [331] * Moving towards #22. Still many packets to study. But we'll get there.

--- src/game/GossipDef.cpp
+++ src/game/GossipDef.cpp
@@ -1,6 +1,5 @@
-/* Quest.h
+/* GossipDef.cpp
  *
- * Copyright (C) 2004 Wow Daemon
  * Copyright (C) 2005 MaNGOS <https://opensvn.csie.org/traccgi/MaNGOS/trac.cgi/>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -20,12 +19,22 @@
 
 #include "QuestDef.h"
 #include "GossipDef.h"
+#include "ObjectMgr.h"
 #include <string.h>
 
+
+/*
+ *  Gossip Menu Implementation
+ */
 
 GossipMenu::GossipMenu()
 {
 	m_gItemsCount = 0;
+}
+
+GossipMenu::~GossipMenu()
+{
+	ClearMenu();
 }
 
 void GossipMenu::MenuItem(uint8 Icon, std::string Message, bool Coded)
@@ -47,4 +56,43 @@ void GossipMenu::ClearMenu()
 		delete m_gItems[i].m_gMessage;
 
 	m_gItemsCount = 0;
+}
+
+/*
+ *  Quest Menu Implementation
+ */
+
+QuestMenu::QuestMenu()
+{
+	m_qItemsCount = 0;
+}
+
+QuestMenu::~QuestMenu()
+{
+	ClearMenu();
+}
+
+void QuestMenu::QuestItem( uint32 QuestId, uint8 Icon, bool Available)
+{
+    Quest *pQuest = objmgr.GetQuest(QuestId);
+	if (!pQuest) return;
+
+	char* Text = new char[strlen( pQuest->m_qTitle.c_str() ) + 1];
+	strcpy( Text, pQuest->m_qTitle.c_str() );
+
+	m_qItemsCount++;
+	ASSERT( m_qItemsCount < GOSSIP_MAX_MENU_ITEMS  );
+
+	m_qItems[m_qItemsCount - 1].m_qIcon		 = Icon;
+	m_qItems[m_qItemsCount - 1].m_qTitle	 = Text;
+	m_qItems[m_qItemsCount - 1].m_qId		 = QuestId;
+	m_qItems[m_qItemsCount - 1].m_qAvailable = Available;
+}
+
+void QuestMenu::ClearMenu()
+{
+	for (int i=0; i<m_qItemsCount; i++)
+		delete m_qItems[i].m_qTitle;
+
+	m_qItemsCount = 0;
 }
