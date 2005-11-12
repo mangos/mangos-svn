439c4e5f5414df1ae327fc6e43aa606f2952c333
siuolly <siuolly@26674162-90ff-0310-a85b-b64107c8dee9>
2005-11-12 17:15:55 +0000

    * Fixed: VC7 build for previous delivery.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@356 26674162-90ff-0310-a85b-b64107c8dee9

--- src/game/ObjectAccessor.cpp
+++ src/game/ObjectAccessor.cpp
@@ -322,9 +322,8 @@ ObjectAccessor::Update(const uint32  &diff)
 	    CellPair update_cell(standing_cell);
 	    update_cell << 1;
 	    update_cell -= 1;
-	    for(; abs(standing_cell.x_coord - update_cell.x_coord) < 2; update_cell >> 1)
 	    {
-		for(CellPair cell_iter=update_cell; abs(standing_cell.y_coord - cell_iter.y_coord) < 2; cell_iter += 1)
+			for(CellPair cell_iter=update_cell; abs(int(standing_cell.y_coord - cell_iter.y_coord)) < 2; cell_iter += 1)
 		{		    
 		    uint32 cell_id = (cell_iter.y_coord*TOTAL_NUMBER_OF_CELLS_PER_MAP) + cell_iter.x_coord;
 		    if( !marked_cell.test(cell_id) )
