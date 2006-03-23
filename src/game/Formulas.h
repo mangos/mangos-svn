fb6ca168ebed2c6461c3c86f4ebb8656d258db7f
andre <andre@no-mail.dom>
2006-03-23 16:14:29 +0000

    [959] more fixes of honor fields and honor formulas :D

--- src/game/Formulas.h
+++ src/game/Formulas.h
@@ -24,6 +24,74 @@
 namespace MaNGOS
 {
     
+	namespace Honor
+	{
+		inline float DishonorableKillPoints(int level)
+		{
+			if(level <= 29) return  10.0; else
+			if(level == 30) return  11.5; else
+			if(level == 31) return  13.0; else
+			if(level == 32) return  14.5; else
+			if(level == 33) return  16.0; else
+			if(level == 34) return  17.5; else
+			if(level == 35) return  19.0; else
+			if(level == 36) return  21.0; else
+			if(level == 37) return  23.0; else
+			if(level == 38) return  25.0; else
+			if(level == 39) return  27.0; else
+			if(level == 40) return  29.0; else
+			if(level == 41) return  31.0; else
+			if(level == 42) return  34.2; else
+			if(level == 43) return  37.4; else
+			if(level == 44) return  40.7; else
+			if(level == 45) return  43.9; else
+			if(level == 46) return  47.1; else
+			if(level == 47) return  50.3; else
+			if(level == 48) return  53.6; else
+			if(level == 49) return  56.8; else
+			if(level == 50) return  60.0; else
+			if(level == 51) return  64.0; else
+			if(level == 52) return  68.0; else
+			if(level == 53) return  72.0; else
+			if(level == 54) return  76.0; else
+			if(level == 55) return  80.0; else
+			if(level == 56) return  84.0; else
+			if(level == 57) return  88.0; else
+			if(level == 58) return  92.0; else
+			if(level == 59) return  96.0; else
+			if(level == 60) return 100.0; else
+				return 100.0;
+		}
+
+		inline float HonorableKillPoints(int total_kills, int k_rank, int v_rank, int k_level, int v_level, float diff_honor, float diff_level)
+		{			
+			int f = (4 - total_kills) >= 0 ? (4 - total_kills) : 0;
+			int honor_points = ((float)(f * 0.25)*(float)((k_level+(v_rank*5+1))*(1+0.05*diff_honor)*diff_level));
+			return (honor_points <= 400 ? honor_points : 400);
+		}
+
+		inline float HonorableKillPoints2(int victimrank)
+		{
+			if ( victimrank ==  0 ) return 000; else
+			if ( victimrank ==  1 ) return 166; else
+			if ( victimrank ==  2 ) return 175; else
+			if ( victimrank ==  3 ) return 185; else
+			if ( victimrank ==  4 ) return 195; else
+			if ( victimrank ==  5 ) return 205; else
+			if ( victimrank ==  6 ) return 217; else
+			if ( victimrank ==  7 ) return 229; else
+			if ( victimrank ==  8 ) return 241; else
+			if ( victimrank ==  9 ) return 254; else
+			if ( victimrank == 10 ) return 268; else
+			if ( victimrank == 11 ) return 283; else
+			if ( victimrank == 12 ) return 298; else
+			if ( victimrank == 13 ) return 315; else
+			if ( victimrank == 14 ) return 332; else
+				return 400;
+		}
+
+	}
+
     namespace Misc
     {
 	inline bool IsRest(Player *pl)
