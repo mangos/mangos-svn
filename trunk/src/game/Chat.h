9f7beaff685a742fdd0bd42bc72b58e17207e585
sani <sani@26674162-90ff-0310-a85b-b64107c8dee9>
2006-05-02 18:03:05 +0000

    Fixed #108 - setpoi_gmcommand
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@1182 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Chat.h
+++ trunk/src/game/Chat.h
@@ -145,6 +145,7 @@ class ChatHandler
         bool HandleRandomCommand(const char* args);
         bool HandleRunCommand(const char* args);
         bool HandleChangeLevelCommand(const char* args);
+	bool HandleSetPoiCommand(const char* args);
         bool HandleNPCFlagCommand(const char* args);
         bool HandleDisplayIdCommand(const char* args);
         bool HandleFactionIdCommand(const char* args);
