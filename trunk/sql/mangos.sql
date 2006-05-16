faf4252ba8e29c389f3b324338a0acd7a64de6b7
anakin <anakin@26674162-90ff-0310-a85b-b64107c8dee9>
2006-05-16 23:10:49 +0000

    FIX: Import mangos.sql issue
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@1263 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/sql/mangos.sql
+++ trunk/sql/mangos.sql
@@ -107,7 +107,7 @@ DROP TABLE IF EXISTS `areatrigger_tavern`;
 CREATE TABLE `areatrigger_tavern` (
   `id` int(11) unsigned NOT NULL default '0' COMMENT 'Identifier',
   `name` text,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Trigger System';
 
 --
