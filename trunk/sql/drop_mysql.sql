c871e5fa00f10862f4f8d85e28d1dd03b1edad6d
theluda <theluda@26674162-90ff-0310-a85b-b64107c8dee9>
2006-05-18 07:05:22 +0000

    Added SQL files for creating and droping MaNGOS database and user/privileges
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@1278 26674162-90ff-0310-a85b-b64107c8dee9

---
+++ trunk/sql/drop_mysql.sql
@@ -0,0 +1,9 @@
+REVOKE ALL PRIVILEGES ON * . * FROM 'mangos'@ 'localhost';
+
+REVOKE ALL PRIVILEGES ON `mangos` . * FROM 'mangos'@ 'localhost';
+
+REVOKE GRANT OPTION ON `mangos` . * FROM 'mangos'@ 'localhost';
+
+DELETE FROM `user` WHERE CONVERT( User USING utf8 ) = CONVERT( 'mangos' USING utf8 ) AND CONVERT( Host USING utf8 ) = CONVERT( 'localhost' USING utf8 ) ;
+
+DROP DATABASE IF EXISTS `mangos` ;
