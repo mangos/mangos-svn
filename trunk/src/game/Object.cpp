4dc8026641176a5cfcbc6a681bd729443f4e5026
zhyg <zhyg@26674162-90ff-0310-a85b-b64107c8dee9>
2006-05-23 16:21:09 +0000

    fix spell cancel on target dead. other small fix.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@1333 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Object.cpp
+++ trunk/src/game/Object.cpp
@@ -320,11 +320,11 @@ void Object::BuildTeleportAckMsg(WorldPacket *data, float x, float y, float z, f
     *data << uint32(0x800000);
     *data << uint16(0x67EE);
     *data << uint16(0xD1EB);
-    *data << ang;                                           // instead of *data << z;
+    *data << m_orientation;                                           // instead of *data << z;
     *data << x;
     *data << y;
     *data << z;                                             // instead of *data << ang;
-    //*data << ang;
+    *data << ang;
     *data << uint32(0x0);
 }
 
