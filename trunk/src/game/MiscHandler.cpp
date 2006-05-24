c2a429b619ee4689d31061986bdfd53fb2609fbd
zhyg <zhyg@26674162-90ff-0310-a85b-b64107c8dee9>
2006-05-24 04:26:38 +0000

    Fix areatrigger of city and areatrigger of same map. add a table areatrigger_city as areatrigger_tarvern to handle resting. pls add the city areatrigger id to this table.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@1339 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/MiscHandler.cpp
+++ trunk/src/game/MiscHandler.cpp
@@ -997,28 +997,25 @@ void WorldSession::HandleAreaTriggerOpcode(WorldPacket & recv_data)
 
     Script->scriptAreaTrigger( GetPlayer(), pQuest, Trigger_ID );
 
-    Field *fields;
-    QueryResult *result = sDatabase.PQuery("SELECT COUNT(*) FROM `areatrigger_tavern` WHERE `id` = '%u';", Trigger_ID);
-    int cnt = 0;
+    QueryResult *result = sDatabase.PQuery("SELECT * FROM `areatrigger_tavern` WHERE `id` = '%u';", Trigger_ID);
+	if(!result)
+		result = sDatabase.PQuery("SELECT * FROM `areatrigger_city` WHERE `id` = '%u';", Trigger_ID);
     if(result)
     {
-        fields = result->Fetch();
-        cnt = fields[0].GetUInt32();
-
         // player flag 0x20 - resting
-        if ( cnt > 0 )
-        {
-            if(!GetPlayer()->HasFlag(PLAYER_FLAGS, 0x20))
-                GetPlayer()->SetFlag(PLAYER_FLAGS, 0x20);
-        }
+        if(!GetPlayer()->HasFlag(PLAYER_FLAGS, 0x20))
+            GetPlayer()->SetFlag(PLAYER_FLAGS, 0x20);
     }
-    if(cnt == 0 && at)
+    else if(at)
     {
         if(at->mapId == GetPlayer()->GetMapId() )
         {
             WorldPacket movedata;
             _player->BuildTeleportAckMsg(&movedata, at->X, at->Y, at->Z, at->Orientation );
             _player->SendMessageToSet(&movedata,true);
+			_player->SetPosition( closestX, closestY, closestZ, 0.0);
+			_player->BuildHeartBeatMsg(&data); 
+			_player->SendMessageToSet(&data, true);
         }
         else
         {
