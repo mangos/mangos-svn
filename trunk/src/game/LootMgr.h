460953c2613a35c5002eef785cc57a472619d6ca
vladimir <vladimir@26674162-90ff-0310-a85b-b64107c8dee9>
2006-06-05 23:54:16 +0000

    Fixed #469 One more loot item chance fix
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@1488 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/LootMgr.h
+++ trunk/src/game/LootMgr.h
@@ -49,7 +49,7 @@ struct LootItem
         // rand() result range is 0..RAND_MAX where RAND_MAX is implementation difine (at 32-bit OS in most case RAND_MAX = 32767)
         // this is small number for chances writed like xx.xxx (100000 cases)
         // using combined 2 call rand() instead 
-        return itm.chance < (float(rand() % 100) + float(rand() % 1000)/1000.0);
+        return itm.chance <= (float(rand() % 100) + float(rand() % 1000)/1000.0);
     }
 };
 
