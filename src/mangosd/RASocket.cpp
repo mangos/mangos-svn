13460e9986c8e2785afb124781d9948084443962
theluda <theluda@no-mail.dom>
2006-06-07 11:59:04 +0000

    [1508] Fixed #511 --- CliRunable using the correct database.

--- src/mangosd/RASocket.cpp
+++ src/mangosd/RASocket.cpp
@@ -140,7 +140,7 @@ void RASocket::OnRead()
                     //if(bLog)Log("User %s",
                     strcpy(szLogin,&buff[5]);
                     //access db now
-                    QueryResult* result = sDatabase.PQuery("SELECT `password`,`gmlevel` FROM `accounts` WHERE `username` = '%s'",szLogin);
+                    QueryResult* result = loginDatabase.PQuery("SELECT `password`,`gmlevel` FROM `account` WHERE `username` = '%s'",szLogin);
                     if(!result)
                     {
                         Sendf("-No such user.\x0d\x0a");
