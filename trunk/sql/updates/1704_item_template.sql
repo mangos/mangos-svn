a758e4de068d0ae6d9247906ff17665affcff182
vladimir <vladimir@26674162-90ff-0310-a85b-b64107c8dee9>
2006-07-07 01:38:32 +0000

    Fixed 1704_item_template.sql sql update :(
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@1705 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/sql/updates/1704_item_template.sql
+++ trunk/sql/updates/1704_item_template.sql
@@ -2,7 +2,7 @@ ALTER TABLE `item_template` ADD COLUMN `BuyCount` TINYINT(3) UNSIGNED NOT NULL D
 ALTER TABLE `npc_vendor` DROP COLUMN `buycount`;
 
 UPDATE `item_template`
-    SET `BuyCount` = (`BuyCount` DIV (`SellPrice` * 4)) WHERE `SellPrice` > 0;
+    SET `BuyCount` = (`BuyPrice` DIV (`SellPrice` * 4)) WHERE `SellPrice` > 0;
 
 UPDATE `item_template`
     SET `BuyCount` = 1 WHERE `BuyCount` = 0;
