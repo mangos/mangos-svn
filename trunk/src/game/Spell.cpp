15f6ff2f2adca838ea0b1a5dc9a6649086f97021
ant009 <ant009@26674162-90ff-0310-a85b-b64107c8dee9>
2006-07-13 12:55:51 +0000

    Set limit of getting into stealth.test it.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@1762 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Spell.cpp
+++ trunk/src/game/Spell.cpp
@@ -1326,7 +1326,34 @@ uint8 Spell::CanCast()
             case SPELL_AURA_MOD_STEALTH:
             {
                 //detect if any mod is in x range.if true,can't steath.FIX ME!
+            if(m_spellInfo->Attributes == 169148432 || m_caster->GetTypeId() != TYPEID_PLAYER)
                 break;
+            CellPair p(MaNGOS::ComputeCellPair(m_caster->GetPositionX(), m_caster->GetPositionY()));
+              Cell cell = RedZone::GetZone(p);
+            cell.data.Part.reserved = ALL_DISTRICT;
+
+            std::list<Unit*> i_data;
+            std::list<Unit*>::iterator itr;
+            MaNGOS::GridUnitListNotifier checker(i_data);
+
+            TypeContainerVisitor<MaNGOS::GridUnitListNotifier, TypeMapContainer<AllObjectTypes> > object_checker(checker);
+            CellLock<GridReadGuard> cell_lock(cell, p);
+            cell_lock->Visit(cell_lock, object_checker, *MapManager::Instance().GetMap(m_caster->GetMapId()));
+            for(itr = i_data.begin();itr != i_data.end();itr++)
+            {
+               if( !(*itr)->isAlive() )
+                  continue;
+               FactionTemplateResolver my_faction(sFactionTemplateStore.LookupEntry(m_caster->getFaction()));
+               FactionTemplateEntry *its_faction = sFactionTemplateStore.LookupEntry((*itr)->getFaction());
+               if (!its_faction) continue;
+               if( my_faction.IsFriendlyTo(its_faction) ) continue;
+               if((*itr)->GetTypeId() != TYPEID_PLAYER && (*itr)->GetDistanceSq(m_caster) < ((Creature*)(*itr))->GetAttackDistance(m_caster))
+               {
+                  castResult = CAST_FAIL_TOO_CLOSE_TO_ENEMY;
+                  break;
+               }
+            }
+
             }
             default:break;
         }
