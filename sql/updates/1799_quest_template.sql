5bb115c09845b27ff7faa40f3e0ef6f27877d3fa
vladimir <vladimir@no-mail.dom>
2006-07-18 01:21:08 +0000

    [1800] Fixed sql update for 1799

--- sql/updates/1799_quest_template.sql
+++ sql/updates/1799_quest_template.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `quest_template` 
     ADD COLUMN `RequiredRepFaction` int(11) unsigned NOT NULL default '0' AFTER `RequiredTradeskill`,
-    ADD COLUMN `RequiredRepValue` int(11) unsigned NOT NULL default '0' AFTER `RequiredRepFaction1`,
+    ADD COLUMN `RequiredRepValue` int(11) unsigned NOT NULL default '0' AFTER `RequiredRepFaction`,
     CHANGE `ReqKillMobId1` `ReqKillMobOrGOId1` int(11) NOT NULL default '0',
     CHANGE `ReqKillMobId2` `ReqKillMobOrGOId2` int(11) NOT NULL default '0',
     CHANGE `ReqKillMobId3` `ReqKillMobOrGOId3` int(11) NOT NULL default '0',
