cd4b8dcb8c75f7048edae0ddda52dd444a2c4fad
sani <sani@no-mail.dom>
2006-08-10 18:16:42 +0000

    [1967] some fixes

--- src/game/Mail.cpp
+++ src/game/Mail.cpp
@@ -78,6 +78,7 @@ void WorldSession::HandleSendMail(WorldPacket & recv_data )
         data << uint32(0);
         data << uint32(4);
         SendPacket(&data);
+	return;
     }
 
     delete result;
