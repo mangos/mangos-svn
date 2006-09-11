0f694e70f453e39e4377cb44e9f5b7b0f1d24994
vladimir <vladimir@26674162-90ff-0310-a85b-b64107c8dee9>
2006-09-11 03:35:57 +0000

    Fixed Unix build after [2190]
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@2193 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Player.h
+++ trunk/src/game/Player.h
@@ -424,7 +424,7 @@ class MANGOS_DLL_SPEC Player : public Unit
         };
 
         void UnsummonPet(Creature* pet = NULL);
-        void Player::UnTamePet(Creature* pet = NULL);
+        void UnTamePet(Creature* pet = NULL);
         void Uncharm();
 
         float GetResistanceBuffMods(SpellSchools school, bool positive) const { return GetFloatValue(positive ? PLAYER_FIELD_RESISTANCEBUFFMODSPOSITIVE+school : PLAYER_FIELD_RESISTANCEBUFFMODSNEGATIVE+school ); }
