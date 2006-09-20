799a832fb42845e2826ace66227a76cda59174ff
theluda <theluda@26674162-90ff-0310-a85b-b64107c8dee9>
2006-09-20 16:03:58 +0000

    MaNGOS 0.5, milestone "Stable Master" completed. Last minute fixes:  * Remove depcrecated files  * Fix build files  * Typo fixes
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@2251 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/sql/realmd.sql
+++ trunk/sql/realmd.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: realmd
 -- ------------------------------------------------------
--- Server version	5.0.22-Debian_0ubuntu6.06-log
+-- Server version	5.0.22-Debian_0ubuntu6.06.2-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
