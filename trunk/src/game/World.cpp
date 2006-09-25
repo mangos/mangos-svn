fc04c0fbe9ab1b8c973f101c8ca9c22aa580b6ce
septimus <septimus@26674162-90ff-0310-a85b-b64107c8dee9>
2006-09-25 14:59:22 +0000

    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@2294 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/World.cpp
+++ trunk/src/game/World.cpp
@@ -209,7 +209,7 @@ void World::SetInitialWorldSettings()
     regen_values[RATE_CREATURE_ELITE_RARE_DAMAGE] = sConfig.GetFloatDefault("Rate.Creature.Elite.RARE.Damage", 1);
     regen_values[RATE_CREATURE_NORMAL_HP] = sConfig.GetFloatDefault("Rate.Creature.Normal.HP", 1);
     regen_values[RATE_CREATURE_ELITE_ELITE_HP] = sConfig.GetFloatDefault("Rate.Creature.Elite.Elite.HP", 1);
-    regen_values[RATE_CREATURE_ELITE_RAREETLITE_HP] = sConfig.GetFloatDefault("Rate.Creature.Elite.RAREELITE.HP", 1);
+    regen_values[RATE_CREATURE_ELITE_RAREELITE_HP] = sConfig.GetFloatDefault("Rate.Creature.Elite.RAREELITE.HP", 1);
     regen_values[RATE_CREATURE_ELITE_WORLDBOSS_HP] = sConfig.GetFloatDefault("Rate.Creature.Elite.WORLDBOSS.HP", 1);
     regen_values[RATE_CREATURE_ELITE_RARE_HP] = sConfig.GetFloatDefault("Rate.Creature.Elite.RARE.HP", 1);
     regen_values[RATE_CREATURE_AGGRO]  = sConfig.GetFloatDefault("Rate.Creature.Aggro", 1);
