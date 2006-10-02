a81df86fda2ad9d31fe8af7dbb344f572ba3e862
vladimir <vladimir@no-mail.dom>
2006-10-02 20:30:43 +0000

    [2344] Update mangos.sql. Starting use new resting place detection in [2343] required DB data update.

--- sql/updates/2343_areatrigger_template.sql
+++ sql/updates/2343_areatrigger_template.sql
@@ -2,13 +2,13 @@ ALTER TABLE `character`
     ADD `logout_time` int(11) NOT NULL DEFAULT '0',
     ADD `is_logout_resting` int(11) NOT NULL DEFAULT '0',
     ADD `rest_bonus` FLOAT NOT NULL DEFAULT '0';
+
 DROP TABLE IF EXISTS `areatrigger_city`;
+
 ALTER TABLE `areatrigger_template` 
-    DROP `name`,
-    DROP `target_map`,
-    ADD `name` text AFTER `id`,
-    ADD `trigger_map` int(11) unsigned NOT NULL DEFAULT '0' AFTER `name`,
+    CHANGE `name` `name` text AFTER `id`,
+    ADD `trigger_map` int(11) unsigned NOT NULL DEFAULT '0' AFTER `name`,    
     ADD `trigger_postion_x` FLOAT NOT NULL DEFAULT '0' AFTER `trigger_map`,
     ADD `trigger_position_y` FLOAT NOT NULL DEFAULT '0' AFTER `trigger_postion_x`,
     ADD `trigger_position_z` FLOAT NOT NULL DEFAULT '0' AFTER `trigger_position_y`,
-    ADD `target_map` int(11) unsigned NOT NULL DEFAULT '0' AFTER `trigger_position_z`;
+    CHANGE `target_map` `target_map` int(11) unsigned NOT NULL DEFAULT '0' AFTER `trigger_position_z`;
