4df3009c2f2b8f85d99fbfa97a4899722dd39f51
vladimir_mangos <vladimir_mangos@no-mail.dom>
2006-10-07 20:38:35 +0000

    [2373] [Note: fixed 2369_creature_template.sql and fix 2372_creature_template.sql (can be applied to fix and not fixed version 2369_creature_template.sql ] Fixing problems with last creature_template sql update.

--- sql/updates/2369_creature_template.sql
+++ sql/updates/2369_creature_template.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `creature_template` 
   CHANGE `level`     `minlevel`  int(3) unsigned default '1',
-  CHANGE `level_max` `maxlevel`  int(3) unsigned default '1',
+  CHANGE `level_max` `maxlevel`  int(3) unsigned default '1' AFTER `minlevel`,
   ADD COLUMN         `minhealth` int(5) unsigned default '0' AFTER `maxlevel`,
   CHANGE `maxhealth` `maxhealth` int(5) unsigned default '0' AFTER `minhealth`,
   ADD COLUMN         `minmana`   int(5) unsigned default '0' AFTER `maxhealth`,
