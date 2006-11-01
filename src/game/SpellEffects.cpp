f59e89d1b716ae58e7a28b786add9eadbb367a5e
vladimir_mangos <vladimir_mangos@no-mail.dom>
2006-11-01 18:33:45 +0000

    [2529] Fixed: not set mana more max mana to caster at drain mana effect.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -353,15 +353,17 @@ void Spell::EffectManaDrain(uint32 i)
     if(curPower < damage)
     {
         unitTarget->SetPower(drain_power,0);
-        if(drain_power == POWER_MANA)
-            m_caster->SetPower(POWER_MANA,uint32(m_caster->GetPower(POWER_MANA)+curPower*tmpvalue));
+        damage = curPower;
     }
     else
-    {
         unitTarget->SetPower(drain_power,curPower-damage);
-        if(drain_power == POWER_MANA)
-            m_caster->SetPower(POWER_MANA,uint32(m_caster->GetPower(POWER_MANA)+damage*tmpvalue));
+
+    if(drain_power == POWER_MANA)
+    {
+        uint32 new_mana = m_caster->GetPower(POWER_MANA)+uint32(damage*tmpvalue);
+        m_caster->SetPower(POWER_MANA,new_mana <= m_caster->GetMaxPower(POWER_MANA) ? new_mana : m_caster->GetMaxPower(POWER_MANA));
     }
+
 }
 
 void Spell::EffectSendEvent(uint32 i)
