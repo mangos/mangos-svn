270eefc94dcf94dfd2371de7b95c83969bde7531
komdori <komdori@no-mail.dom>
2006-11-05 03:23:36 +0000

    [2574] New version of map extractor (part 2).  The code is a mess!, but since many are waiting for it and I'm busy with other things atm, releasing as is.

--- contrib/map_extractor/mpq_libmpq.cpp
+++ contrib/map_extractor/mpq_libmpq.cpp
@@ -1,18 +1,20 @@
 #include "mpq_libmpq.h"
 #include <vector>
+#include "libmpq/common.h"
 
-typedef std::vector<mpq_archive*> ArchiveSet;
+//typedef std::vector<mpq_archive*> ArchiveSet;
+typedef std::vector<MPQArchive*> ArchiveSet;
 ArchiveSet gOpenArchives;
 
 MPQArchive::MPQArchive(const char* filename)
 {
 	int result = libmpq_archive_open(&mpq_a, (unsigned char*)filename);
-	printf("Opening %s\n", filename);
+    printf("Opening %s\n", filename);
 	if(result) {
 		printf("Error opening archive %s\n", filename);
 		return;
 	}
-	gOpenArchives.push_back(&mpq_a);
+	gOpenArchives.push_back(this); //&mpq_a);
 }
 
 void MPQArchive::close()
@@ -29,12 +31,22 @@ MPQFile::MPQFile(const char* filename):
 {
 	for(ArchiveSet::iterator i=gOpenArchives.begin(); i!=gOpenArchives.end();++i)
 	{
+		mpq_archive &mpq_a = (*i)->mpq_a;
 		
-		mpq_archive &mpq_a = **i;
-		int fileno = libmpq_file_number(&mpq_a, filename);
-		if(fileno == LIBMPQ_EFILE_NOT_FOUND)
-			continue;
-		// Found!
+        mpq_hash hash = (*i)->GetHashEntry(filename);
+		uint32 blockindex = hash.blockindex;
+
+        if ((blockindex == 0xFFFFFFFF) || (blockindex == 0)) {
+            continue; //file not found
+        }
+        
+        int fileno = blockindex;
+
+        //int fileno = libmpq_file_number(&mpq_a, filename);
+		//if(fileno == LIBMPQ_EFILE_NOT_FOUND)
+		//	continue;
+
+        // Found!
 		size = libmpq_file_info(&mpq_a, LIBMPQ_FILE_UNCOMPRESSED_SIZE, fileno);
 		// HACK: in patch.mpq some files don't want to open and give 1 for filesize
 		if (size<=1) {
@@ -43,7 +55,9 @@ MPQFile::MPQFile(const char* filename):
 			return;
 		}
 		buffer = new char[size];
-		libmpq_file_getdata(&mpq_a, fileno, (unsigned char*)buffer);
+
+		//libmpq_file_getdata
+        libmpq_file_getdata(&mpq_a, hash, fileno, (unsigned char*)buffer);
 		return;
 
 	}
