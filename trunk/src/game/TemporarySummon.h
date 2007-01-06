622d46d36cf1c0e9fb65ea26db96c387474d4052
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2007-01-06 05:52:44 +0000

    Sorry, forgot add TemporarySummon source files. in [2938]. Add now.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@2941 26674162-90ff-0310-a85b-b64107c8dee9

---
+++ trunk/src/game/TemporarySummon.h
@@ -0,0 +1,45 @@
+/* 
+ * Copyright (C) 2005,2006 MaNGOS <http://www.mangosproject.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef MANGOSSERVER_TEMPSUMMON_H
+#define MANGOSSERVER_TEMPSUMMON_H
+
+#include "Creature.h"
+
+enum TempSummonType
+{
+    TEMPSUMMON_REMOVE_DEAD   = 1,
+    TEMPSUMMON_REMOVE_CORPSE = 2,
+};
+
+class TemporarySummon : public Creature
+{
+    public:
+        TemporarySummon();
+        virtual ~TemporarySummon(){};
+        void Update(uint32 time);
+        void Summon(TempSummonType type, uint32 lifetime);
+        void UnSummon();
+        void setDeathState(DeathState s);
+        void SaveToDB();
+    private:
+        TempSummonType m_type;
+        uint32 m_timer;
+        uint32 m_lifetime;
+};
+#endif
