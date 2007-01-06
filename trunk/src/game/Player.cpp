c214cc9908aee2b7d20883afc02c2e193aa8fd8b
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2007-01-06 05:43:16 +0000

    Fixed: learn lockpicking skill at picklocking spell learning. Patch provided by Greenseed. [Note: sql update 2940_playercreateinfo_skill.sql] Fixed: remove lockpicking skill from playercreateinfo_skill table. Fixed: count quest items received by mail. Patch provided by Leela. Fixed: mangos.sql errors in prev. patch.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@2940 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Player.cpp
+++ trunk/src/game/Player.cpp
@@ -2049,6 +2049,9 @@ void Player::learnSpell(uint16 spell_id)
         case 33391:                                         //Mid riding
             SetSkill(762,150,150);
             break;
+        case 1804:                                          //Pick Lock(Rogue)
+            SetSkill(633,1,this->GetLevel()*5);
+            break;
             // Languages
         case 668: case 669: case 670: case 671:  case 672:  case 813: case 814:
         case 815: case 816: case 817: case 7340: case 7341: case 17737:
