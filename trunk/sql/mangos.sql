d89f132c467aade5d20e9e51ec52b8054a3b212c
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2007-01-14 10:23:33 +0000

    [New sql update: 3017_auctionhouse.sql ] Make guid fields in `auctionhouse` unsigned as used in source code.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@3017 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/sql/mangos.sql
+++ trunk/sql/mangos.sql
@@ -94,13 +94,13 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `auctionhouse`;
 CREATE TABLE `auctionhouse` (
   `id` int(11) unsigned NOT NULL DEFAULT '0',
-  `auctioneerguid` int(11) NOT NULL default '0',
-  `itemguid` int(11) NOT NULL default '0',
+  `auctioneerguid` int(11) unsigned NOT NULL default '0',
+  `itemguid` int(11) unsigned NOT NULL default '0',
   `item_template` int(11) unsigned NOT NULL default '0' COMMENT 'Item Identifier',
-  `itemowner` int(11) NOT NULL default '0',
+  `itemowner` int(11) unsigned NOT NULL default '0',
   `buyoutprice` int(11) NOT NULL default '0',
   `time` bigint(40) NOT NULL default '0',
-  `buyguid` int(11) NOT NULL default '0',
+  `buyguid` int(11) unsigned NOT NULL default '0',
   `lastbid` int(11) NOT NULL default '0',
   `startbid` int(11) NOT NULL default '0',
   `deposit` int(11) NOT NULL default '0',
