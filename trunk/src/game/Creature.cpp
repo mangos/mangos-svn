85b92659d0a5162ba766aa492ac8a6e6b5c011b3
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2007-01-14 16:12:14 +0000

    Change type of output for some server log strings. Patch provided by  urbin.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@3021 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Creature.cpp
+++ trunk/src/game/Creature.cpp
@@ -969,7 +969,7 @@ bool Creature::CreateFromProto(uint32 guidlow,uint32 Entry)
     CreatureInfo const *cinfo = objmgr.GetCreatureTemplate(Entry);
     if(!cinfo)
     {
-        sLog.outString("Error: creature entry %u does not exist.",Entry);
+        sLog.outError("Error: creature entry %u does not exist.",Entry);
         return false;
     }
     uint32 rank = isPet()? 0 : cinfo->rank;
