7c37ff4ebe9c8b869d110aee12f0a05fd976de9d
septim <septim@26674162-90ff-0310-a85b-b64107c8dee9>
2007-01-22 12:54:54 +0000

    FIX: 1. Creature facing on stun and root effect. Thanks to Vadim.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@3067 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/SpellAuras.cpp
+++ trunk/src/game/SpellAuras.cpp
@@ -1544,6 +1544,11 @@ void Aura::HandleAuraModStun(bool apply, bool Real)
     {
         m_target->clearUnitState(UNIT_STAT_STUNDED);
         m_target->RemoveFlag(UNIT_FIELD_FLAGS, 0x40000);
+        Unit* caster = GetCaster();
+        if(caster)  // set creature facing on root effect
+        {
+            m_target->SetUInt64Value (UNIT_FIELD_TARGET,caster->GetGUIDLow());
+        }
 
         // only at real remove aura
         if(Real)
@@ -1668,6 +1673,11 @@ void Aura::HandleAuraModRoot(bool apply, bool Real)
     {
         m_target->clearUnitState(UNIT_STAT_ROOT);
         m_target->RemoveFlag(UNIT_FIELD_FLAGS,(apply_stat<<16));
+        Unit* caster = GetCaster();
+        if(caster)  // set creature facing on root effect
+        {
+            m_target->SetUInt64Value (UNIT_FIELD_TARGET,caster->GetGUIDLow());
+        }
 
         // only at real remove aura
         if(Real)
