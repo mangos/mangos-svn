23b822f6be2b25dfe1a9a817d39c76b326f330bf
vladimir_mangos <vladimir_mangos@no-mail.dom>
2007-02-15 05:33:53 +0000

    [3209] Improvements and fixes for gifts wrapping code. Patches provided by jID and TOM_RUS.

--- src/game/ItemHandler.cpp
+++ src/game/ItemHandler.cpp
@@ -943,15 +943,25 @@ void WorldSession::HandleWrapItemOpcode(WorldPacket& recv_data)
         return;
     }
     
-    //if(item->IsUnique()) // need figure out unique item flags...
-    //{
-    //    _player->SendEquipError( EQUIP_ERR_UNIQUE_CANT_BE_WRAPPED, item, NULL );
-    //    return;
-    //}
+    // maybe not correct check  (it is better than nothing)
+    if(item->GetProto()->MaxCount>0)
+    {
+        _player->SendEquipError( EQUIP_ERR_UNIQUE_CANT_BE_WRAPPED, item, NULL );
+        return;
+    }
 
     sDatabase.BeginTransaction();
     sDatabase.PExecute("INSERT INTO `character_gifts` VALUES ('%u', '%u', '%u', '%u')", GUID_LOPART(item->GetOwnerGUID()), item->GetGUIDLow(), item->GetEntry(), item->GetUInt32Value(ITEM_FIELD_FLAGS));
     item->SetUInt32Value(OBJECT_FIELD_ENTRY, gift->GetUInt32Value(OBJECT_FIELD_ENTRY));
+    
+    switch (item->GetEntry()) {
+        case 5042:  item->SetUInt32Value(OBJECT_FIELD_ENTRY,  5043); break;
+        case 5048:  item->SetUInt32Value(OBJECT_FIELD_ENTRY,  5044); break;
+        case 17303: item->SetUInt32Value(OBJECT_FIELD_ENTRY, 17302); break;
+        case 17304: item->SetUInt32Value(OBJECT_FIELD_ENTRY, 17305); break;
+        case 17307: item->SetUInt32Value(OBJECT_FIELD_ENTRY, 17308); break;
+        case 21830: item->SetUInt32Value(OBJECT_FIELD_ENTRY, 21831); break;
+    }
     item->SetUInt64Value(ITEM_FIELD_GIFTCREATOR, _player->GetGUID());
     item->SetUInt32Value(ITEM_FIELD_FLAGS, 8); // wrapped ?
     item->SetState(ITEM_CHANGED, _player);
