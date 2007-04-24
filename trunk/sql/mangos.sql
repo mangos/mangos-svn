90ddf7c97d71259d4dd335a140714dd0ec15c4c1
TOM_RUS <TOM_RUS@26674162-90ff-0310-a85b-b64107c8dee9>
2007-04-24 21:51:55 +0000

    Forgot update mangos.sql
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@3513 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/sql/mangos.sql
+++ trunk/sql/mangos.sql
@@ -538,6 +538,7 @@ DROP TABLE IF EXISTS `character_spell_cooldown`;
 CREATE TABLE `character_spell_cooldown` (
   `guid` int(11) unsigned NOT NULL default '0' COMMENT 'Global Unique Identifier, Low part',
   `spell` int(11) unsigned NOT NULL default '0' COMMENT 'Spell Identifier',
+  `item` int(11) unsigned NOT NULL default '0' COMMENT 'Item Identifier',
   `time` bigint(20) unsigned NOT NULL default '0',
   PRIMARY KEY  (`guid`,`spell`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
