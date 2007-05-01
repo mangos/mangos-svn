20a5b05af6a76ccbc4caae68057c9af01111157d
vladimir_mangos <vladimir_mangos@no-mail.dom>
2007-05-01 01:04:30 +0000

    [3554] Revert finally [3542/3551]. Fix small typo (but work for me with it also) in 3553_creature_onkill_reputation.sql

--- sql/updates/3553_creature_onkill_reputation.sql
+++ sql/updates/3553_creature_onkill_reputation.sql
@@ -3,4 +3,4 @@ ALTER TABLE `creature_onkill_reputation`
   CHANGE COLUMN `IsTeamAward1` `IsTeamAward1` tinyint(1) default '0',
   CHANGE COLUMN `MaxStanding2` `MaxStanding2` tinyint(1) default '0',
   CHANGE COLUMN `IsTeamAward2` `IsTeamAward2` tinyint(1) default '0',
-  ADD COLUMN `TeamDependent` tinyint(1) unsigned NOT NULL default '0' AFTER `RewOnKillRepValue2;
+  ADD COLUMN `TeamDependent` tinyint(1) unsigned NOT NULL default '0' AFTER `RewOnKillRepValue2`;
