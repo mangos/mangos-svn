b0cfc6c1599119c8160aee0d39e42290ec765dd4
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2007-05-07 18:30:20 +0000

    Fixed: rename in code already renamed in DB field. Patch provided by Alex Dereka.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@3610 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Creature.cpp
+++ trunk/src/game/Creature.cpp
@@ -1223,7 +1223,7 @@ void Creature::DeleteFromDB()
 
     sDatabase.BeginTransaction();
     sDatabase.PExecuteLog("DELETE FROM `creature` WHERE `guid` = '%u'", m_DBTableGuid);
-    sDatabase.PExecuteLog("DELETE FROM `creature_addon` WHERE `entry` = '%u'", m_DBTableGuid);
+    sDatabase.PExecuteLog("DELETE FROM `creature_addon` WHERE `guid` = '%u'", m_DBTableGuid);
     sDatabase.PExecuteLog("DELETE FROM `creature_movement` WHERE `id` = '%u'", m_DBTableGuid);
     sDatabase.PExecuteLog("DELETE FROM `creature_respawn` WHERE `guid` = '%u' AND `instance` = '%u'", m_DBTableGuid, GetInstanceId());
     sDatabase.CommitTransaction();
