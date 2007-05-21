ec2f8287e135eb483682e2bfee2da0b047e8ef0f
uniqueone <uniqueone@26674162-90ff-0310-a85b-b64107c8dee9>
2007-05-21 14:21:24 +0000

    Fix headers
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@3683 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/shared/Auth/Sha1.h
+++ trunk/src/shared/Auth/Sha1.h
@@ -1,15 +1,19 @@
-/****************************************************************************
+/*
+ * Copyright (C) 2005,2006,2007 MaNGOS <http://www.mangosproject.org/>
  *
- * General Object Type File
- * Copyright (c) 2007 Antrix Team
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This file may be distributed under the terms of the Q Public License
- * as defined by Trolltech ASA of Norway and appearing in the file
- * COPYING included in the packaging of this file.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
- * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #ifndef _AUTH_SHA1_H
