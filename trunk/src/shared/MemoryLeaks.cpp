b0505c38d9eb3d0105f1dab0c7735af19dd87712
wyk3d <wyk3d@26674162-90ff-0310-a85b-b64107c8dee9>
2007-05-27 16:39:57 +0000

    Fix for linux compile.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@3740 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/shared/MemoryLeaks.cpp
+++ trunk/src/shared/MemoryLeaks.cpp
@@ -19,16 +19,15 @@
 #include "MemoryLeaks.h"
 #include "Policies/SingletonImp.h"
 
-#if COMPILER == MICROSOFT
-
 INSTANTIATE_SINGLETON_1( MemoryManager ) ;
 
 MemoryManager::MemoryManager( )
 {
+    #if COMPILER == MICROSOFT
     // standard leak check initialization
     //_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     // uncomment to disable Visual Leak Detector from code
     //VLDDisable();
+    #endif
 }
 
-#endif
