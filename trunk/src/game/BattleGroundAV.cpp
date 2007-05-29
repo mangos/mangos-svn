dcf2942cf0694b71b9d3b7370dfd32e9a672f686
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2007-05-29 03:39:12 +0000

    Fixed: restore build at Unix/Linux after [3753]: Fix wrong case-sensitive filenames.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@3754 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/BattleGroundAV.cpp
+++ trunk/src/game/BattleGroundAV.cpp
@@ -19,7 +19,7 @@
 #include "Object.h"
 #include "Player.h"
 #include "BattleGround.h"
-#include "BattlegroundAV.h"
+#include "BattleGroundAV.h"
 #include "Creature.h"
 #include "Chat.h"
 #include "Spell.h"
