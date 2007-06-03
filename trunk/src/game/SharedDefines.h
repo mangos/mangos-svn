934b62cf11d283f37bea532fa3825be032d88108
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2007-06-03 00:51:53 +0000

    Fixed: set unlearn button show state for skills at player loading base at DBC information instead saved data in `data` field. Patch provided by Intaki.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@3794 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/SharedDefines.h
+++ trunk/src/game/SharedDefines.h
@@ -1203,6 +1203,17 @@ inline uint32 SkillByQuestSort(int32 QuestSort)
     return 0;
 }
 
+enum SkillCategory
+{
+    SKILL_CATEGORY_ATTRIBUTES =  5,
+    SKILL_CATEGORY_WEAPON     =  6,
+    SKILL_CATEGORY_CLASS      =  7,
+    SKILL_CATEGORY_ARMOR      =  8,
+    SKILL_CATEGORY_SECONDARY  =  9,                         // secondary professions
+    SKILL_CATEGORY_LANGUAGES  = 10,
+    SKILL_CATEGORY_PROFESSION = 11                          // primary professions
+};
+
 enum TotemCategory
 {
     TC_SKINNING_SKIFE              = 1,
