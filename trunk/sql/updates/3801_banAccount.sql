5a03199cbf3ac903112bbf49da8d287037a90e66
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2007-06-03 21:53:38 +0000

    [Note: 3801_banAccount.sql updated] Fixed: remove key `account`.`idx_banned`, make sql update apply to realmd, fix my typo in mangos.sql :/
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@3804 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/sql/updates/3801_banAccount.sql
+++ trunk/sql/updates/3801_banAccount.sql
@@ -1,16 +1,19 @@
-ALTER TABLE `ip_banned` ADD COLUMN `bandate` INT NOT NULL AFTER `ip`,
-ADD COLUMN `unbandate` INT NOT NULL AFTER `bandate`,
-ADD COLUMN `bannedby` VARCHAR(50) NOT NULL DEFAULT '[Console]' AFTER `unbandate`,
-ADD COLUMN `banreason` VARCHAR(50) NOT NULL DEFAULT 'no reason' AFTER `bannedby`;
-ALTER TABLE `realmd`.`account` DROP COLUMN `banned`;
+ALTER TABLE `realmd`.`ip_banned` 
+    ADD COLUMN `bandate` INT NOT NULL AFTER `ip`,
+    ADD COLUMN `unbandate` INT NOT NULL AFTER `bandate`,
+    ADD COLUMN `bannedby` VARCHAR(50) NOT NULL DEFAULT '[Console]' AFTER `unbandate`,
+    ADD COLUMN `banreason` VARCHAR(50) NOT NULL DEFAULT 'no reason' AFTER `bannedby`;
 
+ALTER TABLE `realmd`.`account` 
+    DROP KEY `idx_banned`,
+    DROP COLUMN `banned`;
 
 --
 -- Table structure for table `account_banned`
 --
 
-DROP TABLE IF EXISTS `account_banned`;
-CREATE TABLE `account_banned` (
+DROP TABLE IF EXISTS `realmd`.`account_banned`;
+CREATE TABLE `realmd`.`account_banned` (
   `id` int(11) NOT NULL COMMENT   'Account id' default '0',
    `bandate` bigint(40) NOT NULL default '0',
   `unbandate` bigint(40) NOT NULL default '0',
@@ -24,7 +27,7 @@ CREATE TABLE `account_banned` (
 -- Dumping data for table `account_banned`
 --
 
-LOCK TABLES `account_banned` WRITE;
-/*!40000 ALTER TABLE `account_banned` DISABLE KEYS */;
-/*!40000 ALTER TABLE `account_banned` ENABLE KEYS */;
+LOCK TABLES `realmd`.`account_banned` WRITE;
+/*!40000 ALTER TABLE `realmd`.`account_banned` DISABLE KEYS */;
+/*!40000 ALTER TABLE `realmd`.`account_banned` ENABLE KEYS */;
 UNLOCK TABLES;
