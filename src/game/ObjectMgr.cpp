1b58a8b2dab91d676ec8f8a0e9c20ef785753bc8
vladimir_mangos <vladimir_mangos@no-mail.dom>
2007-06-23 22:28:53 +0000

    [3931] Request confirmation at primary profession first rank learn. Patch provided by TOM_RUS.

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -3053,6 +3053,20 @@ bool ObjectMgr::IsProfessionSpell(uint32 spellId)
     return IsProfessionSkill(skill);
 }
 
+bool ObjectMgr::IsPrimaryProfessionSpell(uint32 spellId)
+{
+    SpellEntry const *spellInfo = sSpellStore.LookupEntry(spellId);
+    if(!spellInfo)
+        return false;
+
+    if(spellInfo->Effect[1] != SPELL_EFFECT_SKILL)
+        return false;
+
+    uint32 skill = spellInfo->EffectMiscValue[1];
+
+    return IsPrimaryProfessionSkill(skill);
+}
+
 void ObjectMgr::LoadReputationOnKill()
 {
     uint32 count = 0; 
