4b914b8b22233072c69b3d9cb1e0c0cdfe38bcf0
palf4711 <palf4711@no-mail.dom>
2007-07-07 13:36:18 +0000

    [4040] Small code cleanup

--- src/game/RefManager.h
+++ src/game/RefManager.h
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C) 2005,2006,2007 MaNGOS <http://www.mangosproject.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #ifndef _REFMANAGER_H
 #define _REFMANAGER_H
 //=====================================================
@@ -7,21 +25,21 @@
 
 template <class TO, class FROM> class RefManager : public LinkedListHead
 {
-private:
-    void clearReferences()
-    {
-        LinkedListElement* ref;
-        while((ref = getFirst()) != NULL) {
-            ((BaseReference*) ref)->invalidate();
-            ref->delink(); // the delink might be already done by invalidate(), but doing it here again does not hurt and insures an empty list
-        }
-    }
 public:
     RefManager() { }
     virtual ~RefManager() { clearReferences(); }
 
     Reference<TO, FROM>* getFirst() { return ((Reference<TO, FROM>* ) LinkedListHead::getFirst()); }
     Reference<TO, FROM>* getLast() { return ((Reference<TO, FROM>* ) LinkedListHead::getLast()); }
+
+    void clearReferences()
+    {
+        LinkedListElement* ref;
+        while((ref = getFirst()) != NULL) {
+            ((Reference<TO, FROM>*) ref)->invalidate();
+            ref->delink(); // the delink might be already done by invalidate(), but doing it here again does not hurt and insures an empty list
+        }
+    }
 };
 
 //=====================================================
