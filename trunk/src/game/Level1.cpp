ddf6b209b6440dfe1bbcc160986d19712d1ce8ba
alex_dereka <alex_dereka@26674162-90ff-0310-a85b-b64107c8dee9>
2007-07-10 11:52:49 +0000

    Fixed: buff size in some packets.patch by m17.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@4063 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Level1.cpp
+++ trunk/src/game/Level1.cpp
@@ -1308,7 +1308,7 @@ bool ChatHandler::HandleModifyMountCommand(const char* args)
     chr->SetUInt32Value( UNIT_FIELD_FLAGS , 0x001000 );
     chr->Mount(mId);
 
-    data.Initialize( SMSG_FORCE_RUN_SPEED_CHANGE, (8+4+4) );
+    data.Initialize( SMSG_FORCE_RUN_SPEED_CHANGE, (8+4+1+4) );
     data.append(chr->GetPackGUID());
     data << (uint32)0;
     data << (uint8)0; //new 2.1.0
