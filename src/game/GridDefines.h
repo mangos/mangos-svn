bb780aea9a93db619c8e00fe2843ad4463980859
vladimir_mangos <vladimir_mangos@no-mail.dom>
2007-07-27 02:54:32 +0000

    [4179] Fixed: build at Unix/Linux (tested at Windows and FreeBSD).

--- src/game/GridDefines.h
+++ src/game/GridDefines.h
@@ -155,7 +155,7 @@ namespace MaNGOS
 
     inline bool IsValidMapCoord(float c)
     {
-        return isfinite(c) && (std::fabs(c) < MAP_HALFSIZE - 0.5);
+        return finite(c) && (std::fabs(c) < MAP_HALFSIZE - 0.5);
     }
 
     inline bool IsValidMapCoord(float x, float y)
