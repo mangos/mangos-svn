4f50a62ac420533ba807abd43452ac8da109143d
theluda <theluda@26674162-90ff-0310-a85b-b64107c8dee9>
2007-08-24 08:03:16 +0000

    * branches/bleach-dev/, branches/theluda-dev: Drop 	  old testing branches.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@4380 26674162-90ff-0310-a85b-b64107c8dee9

--- branches/bleach-dev/sql/guilds_members.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `guilds_members`;
-CREATE TABLE IF NOT EXISTS `guilds_members` (
-  `guildId` int(6) unsigned NOT NULL default 0,
-  `memguid`   int(6) NOT NULL default 0,
-  `rank` tinyint(2) unsigned NOT NULL default 0,
-  
-  PRIMARY KEY  (`guildId`)
-) TYPE=MyISAM AUTO_INCREMENT=44 ;
--- branches/bleach-dev/sql/updates/1856_command.sql
+++
@@ -1,3 +0,0 @@
-insert into command values('shuttdown','3','Syntax: .shuttdown seconds');
-insert into command values('cshuttdown','3','Syntax: .cshuttdown Cancels shuttdown');
-
