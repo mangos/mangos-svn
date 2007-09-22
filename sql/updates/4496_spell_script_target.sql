c5dde25e39595a829f6f7771b9489418ca83c4a3
vladimir_mangos <vladimir_mangos@no-mail.dom>
2007-09-22 23:45:00 +0000

    [4497] * Fixed: remove incidentally added test data in 4496_spell_script_target.sql and mangos.sql.

--- sql/updates/4496_spell_script_target.sql
+++ sql/updates/4496_spell_script_target.sql
@@ -4,18 +4,3 @@ CREATE TABLE `spell_script_target` (
   `type` int(8) unsigned default '0',
   `targetEntry` int(11) default '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Spell System';
-
-INSERT INTO `spell_script_target` VALUES 
-/* CREATURE */
-(11637, 1, 6220),
-(11637, 1, 6218),
-(11637, 1, 6219),
-(12709, 1, 6219),
-(12709, 1, 6220),
-(12709, 1, 6218),
-/* DEAD */
-(31210, 2, 17544),
-(31225, 2, 17768),
-/* GO */
-(33067, 0, 0), 
-(39094, 0, 0);
