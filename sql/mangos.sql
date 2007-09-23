1181140af16b95ba0d2f7640d05c188a36922617
vladimir_mangos <vladimir_mangos@no-mail.dom>
2007-09-23 15:29:29 +0000

    [4499] * [New sql update: 4499_spell_script_target.sql] Small changes in table structure.

--- sql/mangos.sql
+++ sql/mangos.sql
@@ -13686,7 +13686,8 @@ DROP TABLE IF EXISTS `spell_script_target`;
 CREATE TABLE `spell_script_target` (
   `entry` int(6) unsigned NOT NULL,
   `type` int(8) unsigned default '0',
-  `targetEntry` int(11) default '0'
+  `targetEntry` int(11) unsigned default '0',
+  UNIQUE KEY `entry_type_target` (`entry`,`type`,`targetEntry`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Spell System';
 LOCK TABLES `transports` WRITE;
 
