33fa1df32103f44246bce983bdfb54156710bc68
triply <triply@26674162-90ff-0310-a85b-b64107c8dee9>
2007-09-30 08:03:21 +0000

    fixed problem in attached sql file in previous patch. I forgot to delete some lines from there... Thx to GreenSeed for report.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@4531 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/sql/updates/4531_gameobject_template_AB.sql
+++ trunk/sql/updates/4531_gameobject_template_AB.sql
@@ -19,15 +19,3 @@ INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `factio
 (180102, 6, 266, 'Neutral Banner Aura', 114, 32, 5, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
 (180255, 0, 6353, 'ALLIANCE DOOR', 114, 32, 1.58, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ''),
 (180256, 0, 6354, 'HORDE DOOR', 114, 32, 1.57, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
-
-
--- 
--- Dumping data for table `creature_template`
--- 
-
-DELETE FROM `creature_template` WHERE `entry` IN (13116, 13117);
-
-INSERT INTO `creature_template` (`entry`, `modelid_m`, `modelid_f`, `name`, `subname`, `minlevel`, `maxlevel`, `minhealth`, `maxhealth`, `minmana`, `maxmana`, `armor`, `faction`, `npcflag`, `speed`, `rank`, `mindmg`, `maxdmg`, `attackpower`, `baseattacktime`, `rangeattacktime`, `flags`, `dynamicflags`, `size`, `family`, `bounding_radius`, `trainer_type`, `trainer_spell`, `class`, `race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `combat_reach`, `type`, `civilian`, `flag1`, `equipmodel1`, `equipmodel2`, `equipmodel3`, `equipinfo1`, `equipinfo2`, `equipinfo3`, `equipslot1`, `equipslot2`, `equipslot3`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `RacialLeader`, `ScriptName`) VALUES 
-(13116, 13336, 0, 'Alliance Spirit Guide', NULL, 60, 60, 8609, 8609, 7860, 7860, 0, 4, 65, 1.76, 1, 134, 280, 1449, 1175, 1292, 0, 0, 1, 0, 2, 0, 0, 0, 0, 62.016, 85.272, 100, 3.65, 7, 1, 2, 22802, 0, 0, 285346306, 0, 0, 2, 0, 0, 13116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 844, '', 0, 3, 0, ''),
-(13117, 13338, 0, 'Horde Spirit Guide', NULL, 60, 60, 8609, 8609, 7860, 7860, 0, 2, 65, 1.76, 1, 134, 280, 1449, 1175, 1292, 0, 0, 1, 0, 2, 0, 0, 0, 0, 62.016, 85.272, 100, 3.65, 7, 1, 2, 24015, 0, 0, 285346306, 0, 0, 1, 0, 0, 13117, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 844, '', 0, 3, 0, '');
-
