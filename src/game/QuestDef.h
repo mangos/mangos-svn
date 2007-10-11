ad6b7ca90104d0e5c42b8444a1baad2d9ec22feb
vladimir_mangos <vladimir_mangos@no-mail.dom>
2007-10-11 01:04:32 +0000

    [4587] * [New sql update: 4587_character_quest_daily.sql] Implement daily quests support. * Fixed: restore correct mail delivered/unread mail counting at character loading.

--- src/game/QuestDef.h
+++ src/game/QuestDef.h
@@ -51,7 +51,7 @@ enum QuestFailedReasons
     INVALIDREASON_UNKNOWN16            = 16,                //need expantion account
     INVALIDREASON_DONT_HAVE_REQ_ITEMS  = 21,                //2.1.3
     INVALIDREASON_DONT_HAVE_REQ_MONEY  = 23,                //2.1.3
-    INVALIDREASON_UNKNOWN26            = 26,                //"you have completed 10 daily quests today"
+    INVALIDREASON_HAVE_10_DAILY_QUESTS = 26,                //"you have completed 10 daily quests today"
     INVALIDREASON_UNKNOWN27            = 27,                //"You cannot completed quests once you have reached tired time"
 };
 
