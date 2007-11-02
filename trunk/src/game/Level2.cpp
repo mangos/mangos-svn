69a7f1783f362ca67e288cd63eaf22b8d182d97a
apoc_mangos <apoc_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2007-11-02 19:47:34 +0000

    - Corrected my mistake in filters in my prev. commit
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@4685 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Level2.cpp
+++ trunk/src/game/Level2.cpp
@@ -188,13 +188,14 @@ bool ChatHandler::HandleTargetObjectCommand(const char* args)
     else
     {
         std::ostringstream eventFilter;
+        eventFilter << " AND (`event` IS NULL ";
         bool initString = true;
 
         for (GameEvent::ActiveEvents::const_iterator itr = ActiveEventsList->begin(); itr != ActiveEventsList->end(); ++itr)
         {
             if (initString)
             {
-                eventFilter  <<  " AND (`event` IS NULL OR `event` IN (" <<*itr;
+                eventFilter  <<  "OR `event` IN (" <<*itr;
                 initString =false;
             }
             else
@@ -203,6 +204,8 @@ bool ChatHandler::HandleTargetObjectCommand(const char* args)
 
         if (!initString)
             eventFilter << "))";
+        else
+            eventFilter << ")";
 
         result = WorldDatabase.PQuery("SELECT `gameobject`.`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, (POW(`position_x` - %f, 2) + POW(`position_y` - %f, 2) + POW(`position_z` - %f, 2)) as `order` FROM `gameobject` LEFT OUTER JOIN `game_event_gameobject` on `gameobject`.`guid`=`game_event_gameobject`.`guid` WHERE `map` = '%i' %s ORDER BY `order` ASC LIMIT 1", m_session->GetPlayer()->GetPositionX(), m_session->GetPlayer()->GetPositionY(), m_session->GetPlayer()->GetPositionZ(), m_session->GetPlayer()->GetMapId(),eventFilter.str().c_str());
     }
