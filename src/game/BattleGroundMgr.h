328e58459687552a7de81ecfd69ec009bb3bf16f
triply <triply@no-mail.dom>
2007-11-09 18:38:08 +0000

    [4702] Fixed great amount of crashes caused by using too high index to array in BattleGroundQueue events. Problem should be fixed now or error will be logged. Now Battleground queues can be used by players that have higher level than 70 and it will not cause crash.

--- src/game/BattleGroundMgr.h
+++ src/game/BattleGroundMgr.h
@@ -77,8 +77,6 @@ class BattleGroundQueue
         void AddPlayer(Player *plr, uint32 bgTypeId);
         void RemovePlayer(uint64 guid, bool decreaseInvitedCount);
 
-        uint32 GetQueueIdByPlayerLevel(uint32 level);
-
         typedef bgqueue<uint64, PlayerQueueInfo> QueuedPlayersMap;
         QueuedPlayersMap m_QueuedPlayers[MAX_BATTLEGROUND_QUEUES];
         typedef std::list<uint64> PlayerGuidsSortedByTimeQueue;
