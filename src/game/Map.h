ebf3fe53f88aafbe5c39927ca1dbd62b2f9c206b
vladimir_mangos <vladimir_mangos@no-mail.dom>
2007-11-17 16:09:51 +0000

    [4724] * Fixed: typo in function name. Patch provided by evandarcz. * Small fixes in character creating code.

--- src/game/Map.h
+++ src/game/Map.h
@@ -225,7 +225,7 @@ class MANGOS_DLL_DECL Map : public MaNGOS::ObjectLevelLockable<Map, ZThread::Mut
 
         bool Instanceable() const { return(i_mapEntry && ((i_mapEntry->map_type == MAP_INSTANCE) || (i_mapEntry->map_type == MAP_RAID))); }
         // NOTE: this duplicate of Instanceable(), but Instanceable() can be changed when BG also will be instanceable
-        bool IsDangeon() const { return(i_mapEntry && ((i_mapEntry->map_type == MAP_INSTANCE) || (i_mapEntry->map_type == MAP_RAID))); }
+        bool IsDungeon() const { return(i_mapEntry && ((i_mapEntry->map_type == MAP_INSTANCE) || (i_mapEntry->map_type == MAP_RAID))); }
         bool IsRaid() const { return(i_mapEntry && (i_mapEntry->map_type == MAP_RAID)); }
 
         virtual bool RemoveBones(uint64 guid, float x, float y);
