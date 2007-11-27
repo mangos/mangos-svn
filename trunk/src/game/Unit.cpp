714dd3a12b9e341da13e4bbd31917008b2d96d1e
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2007-11-27 04:12:09 +0000

    * Fixed: Partly revert wrong protection from DoTs stealth aura in [4774] as wrong.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@4776 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Unit.cpp
+++ trunk/src/game/Unit.cpp
@@ -411,9 +411,11 @@ void Unit::DealDamage(Unit *pVictim, uint32 damage, CleanDamage const* cleanDama
     }
 
     // remove affects at any damage (including 0 damage)
+    RemoveSpellsCausingAura(SPELL_AURA_MOD_STEALTH);
+
+    // remove affects at any non-DoT damage (including 0 damage)
     if( damagetype != DOT)
     {
-        RemoveSpellsCausingAura(SPELL_AURA_MOD_STEALTH);
         RemoveSpellsCausingAura(SPELL_AURA_MOD_INVISIBILITY);
         RemoveSpellsCausingAura(SPELL_AURA_FEIGN_DEATH);
     }
