f36d8c7fb78e58fab12d2c8413c2f2ac29f92d74
vladimir_mangos <vladimir_mangos@no-mail.dom>
2007-12-20 17:08:18 +0000

    [4879] * Implement new loot conditions (skill and quest). Patch provided by NoFantasy. * Fixed: not write broken spell related info in DB log in case rejected learning broken spell, for example, at character loading.

--- src/game/LootMgr.h
+++ src/game/LootMgr.h
@@ -65,8 +65,9 @@ enum AdditionalLootCondition
     CONDITION_ITEM_EQUIPPED         = 3,                    // item_id 0
     CONDITION_ZONEID                = 4,                    // zone_id 0
     CONDITION_REPUTATION_RANK       = 5,                    // faction_id min_rank
-    CONDITION_TEAM                  = 6                     // player_team 0
-    // 469 - Alliance 67 - Horde
+    CONDITION_TEAM                  = 6,                    // player_team 0 (469 - Alliance 67 - Horde)
+    CONDITION_SKILL                 = 7,                    // skill_id skill_value
+    CONDITION_QUESTREWARDED         = 8                     // quest_id 0
 };
 
 class Player;
