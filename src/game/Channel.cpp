52302bfaac76f7c67636aed73b7fa0c41b7cce07
vladimir_mangos <vladimir_mangos@no-mail.dom>
2007-12-26 23:02:30 +0000

    [4929] * Allow GM join to LFG channel in any time (ignore RestrictedLfgChannel option state).

--- src/game/Channel.cpp
+++ src/game/Channel.cpp
@@ -80,7 +80,8 @@ void Channel::Join(uint64 p, const char *pass)
 
     if(plr)
     {
-        if(HasFlag(CHANNEL_FLAG_LFG) && sWorld.getConfig(CONFIG_RESTRICTED_LFG_CHANNEL) && 
+        if(HasFlag(CHANNEL_FLAG_LFG) && 
+            sWorld.getConfig(CONFIG_RESTRICTED_LFG_CHANNEL) && plr->GetSession()->GetSecurity() == SEC_PLAYER &&
             (plr->GetGroup() || plr->m_lookingForGroup.Empty()) )
         {
             MakeNotInLfg(&data);
