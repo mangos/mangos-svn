4e10813554110f4e59f26d8de37e31b590fa366c
vladimir_mangos <vladimir_mangos@no-mail.dom>
2008-01-03 14:33:41 +0000

    [4970] * Kick player at wrong size movement packet, that can be possible only if used cheating tool.

--- src/game/MovementHandler.cpp
+++ src/game/MovementHandler.cpp
@@ -178,7 +178,9 @@ void WorldSession::HandleMovementOpcodes( WorldPacket & recv_data )
 
     if(recv_data.size() != recv_data.rpos())
     {
-        sLog.outError("MovementHandler: player %s (guid %d) sent a packet (opcode %u) that is %u bytes larger than it should be! Sneaky little devil isn't he..", _player->GetName(), _player->GetGUIDLow(), recv_data.GetOpcode(), recv_data.size() - recv_data.rpos());
+        sLog.outError("MovementHandler: player %s (guid %d) sent a packet (opcode %u) that is %u bytes larger than it should be. Kicked as cheater.", _player->GetName(), _player->GetGUIDLow(), recv_data.GetOpcode(), recv_data.size() - recv_data.rpos());
+        KickPlayer();
+        return;
     }
 
     if (!MaNGOS::IsValidMapCoord(movementInfo.x, movementInfo.y))
