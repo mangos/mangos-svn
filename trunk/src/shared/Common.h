37e7943c7514661b47f3b2071d43cec2e51e5e97
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2008-01-10 20:59:24 +0000

    * Switch from 2.3.0 to 2.3.2 client version support. No big changes, but you need use new DBC files, and map regeneration recommended. Patch provided by Dereka.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@5005 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/shared/Common.h
+++ trunk/src/shared/Common.h
@@ -45,9 +45,9 @@
 
 // we need to stick to 1 version or half of the stuff will work for someone
 // others will not and opposite
-// will only support WoW and WoW:TBC 2.3.0 client build 7561...
+// will only support WoW and WoW:TBC 2.3.2 client build 7741...
 
-#define EXPECTED_MANGOS_CLIENT_BUILD        {7561, 0}
+#define EXPECTED_MANGOS_CLIENT_BUILD        {7741, 0}
 
 // must be the first thing to include for it to work
 #include "MemoryLeaks.h"
