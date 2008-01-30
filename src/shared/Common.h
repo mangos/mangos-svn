7ad97160f82903770d55067108f9d168df85b344
vladimir_mangos <vladimir_mangos@no-mail.dom>
2008-01-30 22:31:24 +0000

    [5111] * Switch to 2.3.3 version support. Note: you need update DBC files.

--- src/shared/Common.h
+++ src/shared/Common.h
@@ -45,9 +45,9 @@
 
 // we need to stick to 1 version or half of the stuff will work for someone
 // others will not and opposite
-// will only support WoW and WoW:TBC 2.3.2 client build 7741...
+// will only support WoW and WoW:TBC 2.3.3 client build 7799...
 
-#define EXPECTED_MANGOS_CLIENT_BUILD        {7741, 0}
+#define EXPECTED_MANGOS_CLIENT_BUILD        {7799, 0}
 
 // must be the first thing to include for it to work
 #include "MemoryLeaks.h"
