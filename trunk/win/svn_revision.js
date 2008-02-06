6a8067c6a8a4e699166d511965619ba760b1ce1a
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2008-02-06 00:26:58 +0000

    * Call cscript.exe for svn_revision.js execute. This solve problem in case another program associated with .js files. Patch provided by TOM_RUS. * Re-generate svn_revision.h only and always at revision changes, rebuild dependent files also only svn_revision.h changes/created. Re-build problem with not existed svn_revision.h also solved.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@5153 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/win/svn_revision.js
+++ trunk/win/svn_revision.js
@@ -1,5 +1,4 @@
 var FSO = WScript.CreateObject("Scripting.FileSystemObject");
-var OutputFile = FSO.CreateTextFile("..\\src\\shared\\svn_revision.h", true);
 var EntriesFile = FSO.OpenTextFile(".svn\\entries", 1, false);
 
 EntriesFile.SkipLine();
@@ -18,13 +17,31 @@ EntriesFile.SkipLine();
 
 date_time = EntriesFile.ReadLine();
 
-OutputFile.WriteLine("#ifndef __SVN_REVISION_H__");
-OutputFile.WriteLine("#define __SVN_REVISION_H__");
-OutputFile.WriteLine(" #define SVN_REVISION \"" + revision + "\"");
-OutputFile.WriteLine(" #define SVN_DATE \"" + date_time.substr(0, 10) + "\"");
-OutputFile.WriteLine(" #define SVN_TIME \"" + date_time.substr(11, 8) + "\"");
-OutputFile.WriteLine("#endif // __SVN_REVISION_H__");
+var newData;
 
-OutputFile.Close();
+newData  = "#ifndef __SVN_REVISION_H__\n";
+newData += "#define __SVN_REVISION_H__\n";
+newData += " #define SVN_REVISION \"" + revision + "\"\n";
+newData += " #define SVN_DATE \"" + date_time.substr(0, 10) + "\"\n";
+newData += " #define SVN_TIME \"" + date_time.substr(11, 8) + "\"\n";
+newData += "#endif // __SVN_REVISION_H__\n";
+
 EntriesFile.Close();
+
+var oldData = "";
+
+if( FSO.FileExists("..\\src\\shared\\svn_revision.h") )
+{
+  var HeaderFile = FSO.OpenTextFile("..\\src\\shared\\svn_revision.h", 1, false);
+  var oldData = HeaderFile.ReadAll();
+  HeaderFile.Close();
+}
+
+if(oldData != newData)
+{
+  var OutputFile = FSO.CreateTextFile("..\\src\\shared\\svn_revision.h", true);
+  OutputFile.Write(newData);
+  OutputFile.Close();
+}
+
 
