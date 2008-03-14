c7a83a802e52aafde58c75f6ab31a8fb14274e0c
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2008-03-14 04:16:22 +0000

    * Fixed: typo in [5416] that not let correctly apply damage bonus.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@5421 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/SpellEffects.cpp
+++ trunk/src/game/SpellEffects.cpp
@@ -427,7 +427,7 @@ void Spell::EffectSchoolDMG(uint32 /*i*/)
                 else if(m_spellInfo->SpellFamilyFlags & 0x100000000LL)
                 {
                     int32 base =rand32(uint32(m_caster->GetWeaponDamageRange(RANGED_ATTACK, MINDAMAGE)),uint32(m_caster->GetWeaponDamageRange(RANGED_ATTACK, MAXDAMAGE)));
-                    damage += int32(base/m_caster->GetAttackTime(RANGED_ATTACK)*2800 + m_caster->GetTotalAttackPowerValue(RANGED_ATTACK)*0.2f);
+                    damage += int32(float(base)/m_caster->GetAttackTime(RANGED_ATTACK)*2800 + m_caster->GetTotalAttackPowerValue(RANGED_ATTACK)*0.2f);
                 }
                 //Explosive Trap Effect
                 else if(m_spellInfo->SpellFamilyFlags & 0x00000004)
