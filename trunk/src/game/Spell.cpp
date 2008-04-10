504621e52a1c0c859982554b775399608797a30f
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2008-04-10 18:37:17 +0000

    * Fixed: not all ranged spells interrupted at move. Patch provided by thenecromancer.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@5591 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Spell.cpp
+++ trunk/src/game/Spell.cpp
@@ -2889,7 +2889,7 @@ uint8 Spell::CanCast(bool strict)
     {
         // skip stuck spell to allow use it in falling case and apply spell limitations at movement
         if( (!((Player*)m_caster)->HasMovementFlags(MOVEMENTFLAG_FALLING) || m_spellInfo->Effect[0] != SPELL_EFFECT_STUCK) &&
-            (IsAutoRepeat() || m_rangedShoot || (m_spellInfo->AuraInterruptFlags & AURA_INTERRUPT_FLAG_NOT_SEATED) != 0) )
+            (IsAutoRepeat() || (m_spellInfo->AuraInterruptFlags & AURA_INTERRUPT_FLAG_NOT_SEATED) != 0) )
             return SPELL_FAILED_MOVING;
     }
 
