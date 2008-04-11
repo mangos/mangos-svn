642c239514d6843d9cc325362e03c63aaca0ddb3
vladimir_mangos <vladimir_mangos@no-mail.dom>
2008-04-11 22:08:37 +0000

    [5602] * Fixed: delete some additional data at creature/gameobject deleted, write in DB changes log. Patch provided by SerJoga.

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -1400,6 +1400,8 @@ void Creature::DeleteFromDB()
     WorldDatabase.PExecuteLog("DELETE FROM creature WHERE guid = '%u'", m_DBTableGuid);
     WorldDatabase.PExecuteLog("DELETE FROM creature_addon WHERE guid = '%u'", m_DBTableGuid);
     WorldDatabase.PExecuteLog("DELETE FROM creature_movement WHERE id = '%u'", m_DBTableGuid);
+    WorldDatabase.PExecuteLog("DELETE FROM game_event_creature WHERE guid = '%u'", m_DBTableGuid);
+    WorldDatabase.PExecuteLog("DELETE FROM game_event_model_equip WHERE guid = '%u'", m_DBTableGuid);
     WorldDatabase.CommitTransaction();
 }
 
