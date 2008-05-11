6c482580bd9486427a5e744d495dd8ff5e02c48b
vladimir_mangos <vladimir_mangos@no-mail.dom>
2008-05-11 23:00:44 +0000

    [5811] * Implement aura 250 (use in single item spell) as clone SPELL_AURA_MOD_INCREASE_HEALTH. Restore item effect work after client switch.. Patch provided by Arthorius.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -302,7 +302,7 @@ pAuraHandler AuraHandler[TOTAL_AURAS]=
     &Aura::HandleUnused,                                    //247 unused
     &Aura::HandleNULL,                                      //248 SPELL_AURA_MOD_COMBAT_RESULT
     &Aura::HandleNULL,                                      //249
-    &Aura::HandleNULL,                                      //250 increase health
+    &Aura::HandleAuraModIncreaseHealth,                     //250 SPELL_AURA_MOD_INCREASE_HEALTH_2
     &Aura::HandleNULL                                       //251 SPELL_AURA_MOD_ENEMY_DODGE
 };
 
