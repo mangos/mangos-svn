cfdbd87cfef58cc57e73abcc75843d0a01b2050d
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2008-05-19 06:07:05 +0000

    * Fixed: Speedup some often called checks in AI. Patch provided by edoz.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@5895 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/AggressorAI.cpp
+++ trunk/src/game/AggressorAI.cpp
@@ -123,9 +123,9 @@ AggressorAI::UpdateAI(const uint32 /*diff*/)
 
     i_victimGuid = i_creature.getVictim()->GetGUID();
 
-    if( i_creature.IsWithinDistInMap(i_creature.getVictim(), ATTACK_DISTANCE))
+    if( i_creature.isAttackReady() )
     {
-        if( i_creature.isAttackReady() )
+        if( i_creature.IsWithinDistInMap(i_creature.getVictim(), ATTACK_DISTANCE))
         {
             i_creature.AttackerStateUpdate(i_creature.getVictim());
             i_creature.resetAttackTimer();
