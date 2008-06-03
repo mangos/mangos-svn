b8bc30518f9cc818532e25b2b2789fdc89356f2d
dislord_rus <dislord_rus@26674162-90ff-0310-a85b-b64107c8dee9>
2008-06-03 15:46:35 +0000

    * Show armor penetration in client
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@6040 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/SpellAuras.cpp
+++ trunk/src/game/SpellAuras.cpp
@@ -5024,6 +5024,10 @@ void Aura::HandleModTargetResistance(bool apply, bool Real)
         return;
     // applied to damage as HandleNoImmediateEffect in Unit::CalcAbsorbResist and Unit::CalcArmorReducedDamage
 
+    // show armor penetration
+    if (m_target->GetTypeId() == TYPEID_PLAYER && (m_modifier.m_miscvalue & 1))
+        m_target->ApplyModInt32Value(PLAYER_FIELD_MOD_TARGET_PHYSICAL_RESISTANCE,m_modifier.m_amount, apply);
+
     // show as spell penetration only full spell penetration bonuses (all resistances except armor and holy
     if (m_target->GetTypeId() == TYPEID_PLAYER && (m_modifier.m_miscvalue & 124)==124)
         m_target->ApplyModInt32Value(PLAYER_FIELD_MOD_TARGET_RESISTANCE,m_modifier.m_amount, apply);
