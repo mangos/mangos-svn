35d4f48a28fceabd2e4b2e2aecd74e2f3eb428d5
hunuza <hunuza@26674162-90ff-0310-a85b-b64107c8dee9>
2008-06-29 20:27:00 +0000

    * Fixed build of ad on *NIX systems
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@6202 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/contrib/extractor/System.cpp
+++ trunk/contrib/extractor/System.cpp
@@ -3,7 +3,12 @@
 #include <stdio.h>
 #include <deque>
 #include <set>
+
+#ifdef WIN32
 #include "direct.h"
+#else
+#include <sys/stat.h>
+#endif
 
 #include "dbcfile.h"
 #include "mpq_libmpq.h"
@@ -33,7 +38,7 @@ static char* const langs[]={"deDE", "enUS", "enGB", "frFR", "esES", "zhCN", "zhT
 void CreateDir( const std::string& Path )
 {
     #ifdef WIN32
-     _mkdir( Path.c_str());
+    _mkdir( Path.c_str());
     #else
     mkdir( Path.c_str(), 0777 );
     #endif
