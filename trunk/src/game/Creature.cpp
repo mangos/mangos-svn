50da9e27b7721ec04e0cd7fb17b78e384d5a5666
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2008-07-03 19:27:42 +0000

    * Fixed: make creature always have melee weapon in ready state (if have) as expected. Patch provided by megamage.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@6223 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Creature.cpp
+++ trunk/src/game/Creature.cpp
@@ -188,7 +188,8 @@ bool Creature::UpdateEntry(uint32 Entry, uint32 team, const CreatureData *data )
     SetDisplayId(display_id);
     SetNativeDisplayId(display_id);
     SetByteValue(UNIT_FIELD_BYTES_0, 2, minfo->gender);
-    SetByteValue(UNIT_FIELD_BYTES_2, 0, SHEATH_STATE_UNARMED );
+    // creatures always have melee weapon ready if any
+    SetByteValue(UNIT_FIELD_BYTES_2, 0, SHEATH_STATE_MELEE );
     SetByteValue(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_AURAS );
 
     SetFloatValue(UNIT_FIELD_BOUNDINGRADIUS,minfo->bounding_radius);
