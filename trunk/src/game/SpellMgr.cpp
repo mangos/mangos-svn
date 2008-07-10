d05fbaab76c5eba5856e41c7dbcad54179bdfb16
arrai <arrai@26674162-90ff-0310-a85b-b64107c8dee9>
2008-07-10 13:49:36 +0000

    * Fixed names in emote texts for pets * Fixed sending animation 0 in emotes * Detect SPELL_AURA_MOD_SKILL auras with negative values as a negative effect
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@6258 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/SpellMgr.cpp
+++ trunk/src/game/SpellMgr.cpp
@@ -435,6 +435,10 @@ bool IsPositiveEffect(uint32 spellId, uint32 effIndex)
                     if(spellproto->EffectBasePoints[effIndex]+int32(spellproto->EffectBaseDice[effIndex]) < 0)
                         return false;
                     break;
+                case SPELL_AURA_MOD_SKILL:
+                    if(spellproto->EffectBasePoints[effIndex]+int32(spellproto->EffectBaseDice[effIndex]) < 0)
+                        return false;
+                    break;
                 default:
                     break;
             }
