8c4b4ed17f9e4cdec47cdf76b4dc5d5e1c1e4ebe
hunuza <hunuza@no-mail.dom>
2008-07-12 09:37:03 +0000

    [6271] * Fixed: pet's initial name should be creature family not creature name

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -932,7 +932,13 @@ bool Pet::CreateBaseAtCreature(Creature* creature)
     SetUInt32Value(UNIT_FIELD_PETNEXTLEVELEXP, uint32((MaNGOS::XP::xp_to_level(creature->getLevel()))/4));
     SetUInt32Value(UNIT_FIELD_FLAGS, UNIT_FLAG_PVP_ATTACKABLE);
     SetUInt32Value(UNIT_NPC_FLAGS , 0);
-    SetName(creature->GetName());
+
+    CreatureFamilyEntry const* cFamily = sCreatureFamilyStore.LookupEntry(creature->GetCreatureInfo()->family);
+    if( char* familyname = cFamily->Name[sWorld.GetDBClang()] )
+        SetName(familyname);
+    else
+        SetName(creature->GetName());
+
     m_loyaltyPoints = 1000;
     if(cinfo->type == CREATURE_TYPE_BEAST)
     {
