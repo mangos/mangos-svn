a328946649664fe169d73e271a7a5805adfb19ab
vladimir_mangos <vladimir_mangos@26674162-90ff-0310-a85b-b64107c8dee9>
2008-07-14 09:25:28 +0000

    * Fixed: use mask values instead indexes in TeleportToOptions. Wrong enum in [6299]. Patch provided donshine.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@6300 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Player.h
+++ trunk/src/game/Player.h
@@ -794,11 +794,11 @@ enum DuelCompleteType
 
 enum TeleportToOptions
 {
-    TELE_TO_GM_MODE,
-    TELE_TO_NOT_LEAVE_TRANSPORT,
-    TELE_TO_NOT_LEAVE_COMBAT,
-    TELE_TO_NOT_UNSUMMON_PET,
-    TELE_TO_SPELL,
+    TELE_TO_GM_MODE             = 0x01,
+    TELE_TO_NOT_LEAVE_TRANSPORT = 0x02,
+    TELE_TO_NOT_LEAVE_COMBAT    = 0x04,
+    TELE_TO_NOT_UNSUMMON_PET    = 0x08,
+    TELE_TO_SPELL               = 0x10,
 };
 
 /// Type of environmental damages
