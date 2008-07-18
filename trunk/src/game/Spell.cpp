b3f6493316f4c4d27a59c4f7288a51e3d2d501d4
hunuza <hunuza@26674162-90ff-0310-a85b-b64107c8dee9>
2008-07-18 12:49:40 +0000

    * Fixed: deal damage caused by split damage auras by real caster (to fix threat) * Fixed: use pet as original caster of summon stun to fix threat (should fix problem where pet attacked owner)
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@6330 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Spell.cpp
+++ trunk/src/game/Spell.cpp
@@ -3759,7 +3759,7 @@ uint8 Spell::CanCast(bool strict)
                     if (m_caster->GetTypeId()==TYPEID_PLAYER && m_caster->getClass()==CLASS_WARLOCK)
                     {
                         if (strict)                         //starting cast, trigger pet stun (cast by pet so it doesn't attack player)
-                            pet->CastSpell(pet, 32752, true, NULL, NULL, m_caster->GetGUID());
+                            pet->CastSpell(pet, 32752, true, NULL, NULL, pet->GetGUID());
                     }
                     else
                         return SPELL_FAILED_ALREADY_HAVE_SUMMON;
