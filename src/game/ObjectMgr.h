6c3311b53aedced5224b8a9606e1d5b68bd2af27
vladimir_mangos <vladimir_mangos@no-mail.dom>
2008-08-27 04:05:18 +0000

    [6487] * Implement flight mount transformation at apply zone effect for spell 40214. Use more generic code in mount apply.

--- src/game/ObjectMgr.h
+++ src/game/ObjectMgr.h
@@ -295,7 +295,7 @@ class ObjectMgr
         static CreatureInfo const *GetCreatureTemplate( uint32 id );
         CreatureModelInfo const *GetCreatureModelInfo( uint32 modelid );
         CreatureModelInfo const* GetCreatureModelRandomGender(uint32 display_id);
-        uint32 ChooseDisplayId(uint32 team, const CreatureInfo *cinfo, const CreatureData *data);
+        uint32 ChooseDisplayId(uint32 team, const CreatureInfo *cinfo, const CreatureData *data = NULL);
         EquipmentInfo const *GetEquipmentInfo( uint32 entry );
         static CreatureDataAddon const *GetCreatureAddon( uint32 lowguid )
         {
