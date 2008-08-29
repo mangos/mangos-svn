9abcacc411727c98505511b28f8107fa0e200bf8
arrai <arrai@26674162-90ff-0310-a85b-b64107c8dee9>
2008-08-29 12:27:21 +0000

    * Fixed creatures stucking at 33% of their HP
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@6504 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Creature.cpp
+++ trunk/src/game/Creature.cpp
@@ -408,6 +408,10 @@ void Creature::Update(uint32 diff)
                 m_AI_locked = false;
             }
 
+            // creature can be dead after UpdateAI call
+            // CORPSE/DEAD state will processed at next tick (in other case death timer will be updated unexpectedly)
+            if(!isAlive())
+                break;
             if(m_regenTimer > 0)
             {
                 if(diff >= m_regenTimer)
