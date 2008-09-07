520b77ae734fefcd6f2a23cca3aacd98de361897
hunuza <hunuza@26674162-90ff-0310-a85b-b64107c8dee9>
2008-09-07 18:20:14 +0000

    * Fixed: hide cloak/helm for corpses if option set in interface options. * Fixed: set guild field for corpses to show correct tabard. Patch provided by thenecromancer.
    git-svn-id: http://mangos.svn.sourceforge.net/svnroot/mangos@6561 26674162-90ff-0310-a85b-b64107c8dee9

--- trunk/src/game/Corpse.h
+++ trunk/src/game/Corpse.h
@@ -35,6 +35,17 @@ enum CorpseType
 // Value equal client resurrection dialog show radius.
 #define CORPSE_RECLAIM_RADIUS 39
 
+enum CorpseFlags
+{
+    CORPSE_FLAG_NONE        = 0x00,
+    CORPSE_FLAG_BONES       = 0x01,
+    CORPSE_FLAG_UNK1        = 0x02,
+    CORPSE_FLAG_UNK2        = 0x04,
+    CORPSE_FLAG_HIDE_HELM   = 0x08,
+    CORPSE_FLAG_HIDE_CLOAK  = 0x10,
+    CORPSE_FLAG_LOOTABLE    = 0x20
+};
+
 class Corpse : public WorldObject
 {
     public:
